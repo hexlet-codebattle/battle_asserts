f0f5bfa0b56ec109457735181243fd28395a1017
mokevnin <mokevnin@gmail.com>
2014-01-20 23:15:46 +0400

    fix quoting

--- lib/battle_asserts/syntax_checker.rb
+++ lib/battle_asserts/syntax_checker.rb
@@ -2,34 +2,9 @@
 module BattleAsserts
   module SyntaxChecker
     def self.ruby(expression, &block)
-      f = IO.popen("ruby -c -e '#{unescape(expression)}'", :err=>[:child, :out])
+      f = IO.popen("ruby -c -e \"#{expression.gsub('"', '\"')}\"", :err=>[:child, :out])
       yield f.read
       f.close
     end
   end
-end
-
-#FIXME extract
-def unescape(string)
-  dup = string.dup
-  escaped_chars = ['n', 't', 'r', 'f', 'v', '0', 'a']
-  escaped_subs = {
-    "n" => "\n",
-    "t" => "\t",
-    "r" => "\r",
-    "f" => "\f",
-    "v" => "\v",
-    "0" => "\0",
-    "a" => "\a"
-  }
-
-  pos = 0
-  while pos < dup.length
-    if dup[pos] == '\\' and escaped_chars.include? dup[pos + 1]
-      dup[pos..(pos + 1)] = escaped_subs[dup[pos + 1]]
-    end
-    pos += 1
-  end
-
-  return dup
 end
