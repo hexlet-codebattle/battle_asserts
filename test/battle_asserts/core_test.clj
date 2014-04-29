c043113d52111048f7c52fac858e29f16f720b92
plugin73 <laplugin73@gmail.com>
2014-04-29 09:02:38 +0000

    replace ruby by clojure

---
+++ test/battle_asserts/core_test.clj
@@ -0,0 +1,7 @@
+(ns battle-asserts.core-test
+  (:require [clojure.test :refer :all]
+            [battle-asserts.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 0))))
