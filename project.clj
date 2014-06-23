2340843abc3f477e4df89e0679bf05442ee6c4b6
Konkov Mikhail <mikhkonkov@gmail.com>
2014-06-24 00:40:50 +0400

    change few issues and add tests

--- project.clj
+++ project.clj
@@ -3,5 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.3.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                [circleci/clj-yaml "0.5.2"]])
+                 [org.clojure/tools.namespace "0.2.4"]
+                 [circleci/clj-yaml "0.5.2"]
+                 [clj-stacktrace "0.2.7"]]
+  :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
+                                      'print-cause-trace)
+                     new (ns-resolve (doto 'clj-stacktrace.repl require)
+                                     'pst)]
+                 (alter-var-root orig (constantly (deref new))))]
+  :source-paths ["src" "source"] 
+  :eval-in :nrepl)
