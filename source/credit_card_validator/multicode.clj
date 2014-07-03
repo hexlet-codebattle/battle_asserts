22a476c4ea2eb0c1924d68ae2d7141b494f5f234
Konkov Mikhail <mikhkonkov@gmail.com>
2014-07-03 20:40:57 +0400

    changed of issue - credit card validation

---
+++ source/credit_card_validator/multicode.clj
@@ -0,0 +1,37 @@
+(ns credit-card-validator.multicode) 
+
+(defn- get-digit 
+  [digit]
+  (let [digit-double (* digit 2)]
+    (if (> digit-double  9)
+      (- digit-double 9)
+      digit-double)))
+
+(defn- change-digits
+  [arr]
+  (let [length (count arr)]
+    (areduce 
+      arr
+      index 
+      acc 
+      []
+      (if (or 
+            (and (even? index) (even? length)) 
+            (and (odd? index) (odd? length)))
+        (conj acc (get-digit (aget arr index)))
+        (conj acc (aget arr index))))))
+
+(defn valid-credit-card?
+  [number]
+  (if (nil? (re-find #"\D+" number))
+    (let [arr-number-card (into-array (map #(read-string (str %)) number))
+          sum (reduce + (change-digits arr-number-card))]
+      (= (mod sum 10) 0))
+    false))
+
+(defn check []
+  (assert (not (valid-credit-card? "1234567890123456")))
+  (assert (valid-credit-card? "4408041234567893"))
+  (assert (not (valid-credit-card? "440804l234567893")))
+  (assert (valid-credit-card? "38520000023237"))
+  (assert (valid-credit-card? "4222222222222")))
