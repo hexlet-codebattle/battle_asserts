8ae07b838527ad779d638b3caa0bc11e61d10549
Dementev Roman <infud@yandex.com>
2014-07-04 16:13:09 +0400

    add issues

--- src/battle_asserts/reader.clj
+++ src/battle_asserts/reader.clj
@@ -17,35 +17,33 @@
   (with-open [wrtr (io/writer file-name)]
     (.write wrtr file-yml)))
 
-(defn- generate-check-for-lang [lang input-dir]
-  (clojure.string/split-lines (clojure.string/replace 
-                                (mc/prettify-code 
-                                  (keyword lang )
-                                  (parse-solution/get-asserts-from-file 
-                                    (str input-dir "multicode.clj"))) 
-                                #"'"   "\""))) 
+(defn- generate-check-for-lang [lang file-name]  
+  (mc/prettify-code 
+    (keyword lang )
+    (parse-solution/get-asserts-from-file 
+      (str "test/battle_solutions/" file-name "_test.clj")))) 
 
-(defn- generate-check-for-langs [source-hash input-dir]
-  (let [langs ((source-hash :multicode_checks) :langs)]
-    (reduce #(assoc %1 (keyword %2) (generate-check-for-lang %2 input-dir)) {} langs)))
+(defn- generate-check-for-langs [source-hash file-name]
+  (let [langs ((or (source-hash :multicode_checks) {}) :langs)]
+    (reduce #(assoc %1 (keyword %2) (generate-check-for-lang %2 file-name)) {} langs)))
 
-(defn- generate-hash [source-hash input-dir]
+(defn- generate-hash [source-hash file-name]
   (dissoc 
     (assoc 
       source-hash 
       :checks 
       (merge 
         (source-hash :checks) 
-        (generate-check-for-langs source-hash input-dir)))
+        (generate-check-for-langs source-hash file-name)))
     :multicode_checks))
 
 (defn- create-yml
-  [file-hash input-dir]
-  (yaml/generate-string  (into {} (reverse (generate-hash file-hash input-dir)))))
+  [file-hash file-name]
+  (yaml/generate-string   (into {} (reverse (generate-hash (into {} (reverse file-hash)) file-name)))))
 
 (defn transform 
   "input file name, output file name"
   [file-name input-dir output-dir]
   (let [input-path (str input-dir file-name ".yml")
         output-path (str output-dir file-name ".yml")]
-    (write-file (create-yml (parse-file (read-file input-path)) input-dir) output-path)))
+    (write-file (create-yml (parse-file (read-file input-path)) file-name) output-path)))
