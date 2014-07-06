14fd20dce1bcfd5f94fb9b4c553b8feaac205c89
Kirill Mokevnin <mokevnin@gmail.com>
2014-07-07 00:57:03 +0400

    add multicode check for build_hash_with_default

---
+++ test/battle_solutions/build_hash_with_default_test.clj
@@ -0,0 +1,13 @@
+(ns battle-solutions.build_hash_with_default_test
+  (:require [clojure.test :refer [deftest]]
+            [battle-asserts.test-helper :refer [assert-equal]]))
+
+(defn generate
+  [v default]
+  (reduce #(assoc %1 %2 default) {} v))
+
+(deftest test-asserts
+  (let []
+    (assert-equal {:draft 0 :completed 0} (generate [:draft :completed] 0))
+    (assert-equal {:one 4 :two 4} (generate [:one :two] 4))))
+
