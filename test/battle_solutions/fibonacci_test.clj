114ad545fa7ea1068e72bd00d023aed65e43cca0
Makapoxa <makarocha@gmail.com>
2014-07-17 08:05:09 +0000

    added fibonacci test + generated issue

---
+++ test/battle_solutions/fibonacci_test.clj
@@ -0,0 +1,17 @@
+(ns battle-solutions.fibonacci-test
+  (:require [clojure.test :refer :all]
+            [battle-asserts.test-helper :refer [assert-equal]]))
+
+(defn fibo-finder [number]
+  (if (= number 0) 0
+    (if (= number 1) 1
+      (+
+        (fibo-finder (- number 1))
+        (fibo-finder (- number 2))))))
+
+(deftest test-asserts
+  (assert-equal 0 (fibo-finder 0))
+  (assert-equal 1 (fibo-finder 1))
+  (assert-equal 3 (fibo-finder 4))
+  (assert-equal 13 (fibo-finder 7))
+  (assert-equal 55 (fibo-finder 10)))
