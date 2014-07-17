27db577c6be6e79b87a8f033ed07cdafc0e940f9
aelaa <aelaau@gmail.com>
2014-07-17 13:58:54 +0400

    Added twenty-one

---
+++ test/battle_solutions/twenty_one_test.clj
@@ -0,0 +1,14 @@
+(ns battle-solutions.twenty-one-test
+  (:require [clojure.test :refer :all]
+            [battle-asserts.test-helper :refer [assert-equal]]))
+
+(defn twenty-one? [n]
+  (= (reduce + n) 21))
+
+
+(deftest test-asserts
+  (assert (twenty-one? '(3 4 5 6 3)))
+  (assert (twenty-one? '(11 10)))
+  (assert (not (twenty-one? '(3 11 10))))
+  (assert (not (twenty-one? '(10 10))))
+  )
