6000b58a7a41fbcddb852ab28e2fe6ad48a1d720
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2014-07-18 13:36:04 +0400

    add parse_http_post_params issue

---
+++ test/battle_solutions/parse_post_form_params_test.clj
@@ -0,0 +1,32 @@
+(ns battle-solutions.parse-post-form-params-test
+  (:require [clojure.test :refer :all]
+            [battle-asserts.test-helper :refer [assert-equal]]
+            [clojure.string :as str]))
+
+(defn- nesting-chain [expr] (re-seq #"[a-z]|\[\]" expr))
+
+(defn- transform [acc [chain v]]
+  (if (= (last chain) "[]")
+    (update-in acc
+               (drop-last chain)
+               (comp (partial into []) concat) [v])
+    (assoc-in acc chain v)))
+
+(defn parse-http-params [s]
+  (->>
+    (str/split s #"&")
+    (map #(str/split % #"="))
+    (map #(vector (nesting-chain (first %)) (second %)))
+    (reduce transform {})))
+
+(deftest test-asserts
+  (let [baby { "a" { "b" "1" } }]
+    (assert-equal baby (parse-http-params "a[b]=1")))
+  (let [easy { "a" { "b" "1" "c" "2" } }]
+    (assert-equal easy (parse-http-params "a[b]=1&a[c]=2")))
+  (let [medium { "a" { "b" "1" "c" "2" "d" { "e" "3" } } "x" ["1" "2"] }]
+    (assert-equal medium (parse-http-params "a[b]=1&a[c]=2&a[d][e]=3&x[]=1&x[]=2")))
+  (let [hard { "a" { "b" "1" "c" "2" "d" { "e" "3" } "f" ["24" "42"] } }]
+    (assert-equal hard (parse-http-params "a[b]=1&a[c]=2&a[d][e]=3&a[f][]=24&a[f][]=42")))
+  (let [nightmare { "a" { "b" { "w" { "y" "3" } } "c" "2" "d" ["1" "2"] } "x" ["1" "2" "3"] }]
+    (assert-equal nightmare (parse-http-params "a[b][w][y]=3&a[c]=2&a[d][]=1&a[d][]=2&x[]=1&x[]=2&x[]=3"))))
