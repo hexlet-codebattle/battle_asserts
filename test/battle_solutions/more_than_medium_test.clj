0e4409a438e2962f5bc41ab712b79ed40c9c871b
Krotos <krotos2112@gmail.com>
2014-07-21 13:39:52 +0400

    Added more-than-medium

---
+++ test/battle_solutions/more_than_medium_test.clj
@@ -0,0 +1,21 @@
+(ns battle-solutions.more-than-medium-test
+  (:require [clojure.test :refer :all]
+            [battle-asserts.test-helper :refer [assert-equal]]))
+
+(defn more-than-medium
+  [string]
+  (let [words (re-seq #"\w+" string)
+        average (/ (reduce +
+                           (map #(count %)
+                                words))
+                   (count words))]
+
+    (vec
+     (filter #(> (count %) average)
+             words))))
+
+
+(deftest test-asserts
+  (assert-equal ["This" "sample" "string"] (more-than-medium "This is a sample string"))
+  (assert-equal ["another" "sample"] (more-than-medium "Some another sample"))
+  (assert-equal [] (more-than-medium "Do, do, do, do... do it!")))
