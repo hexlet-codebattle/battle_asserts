5133c4e14a7c4855657ce0e609ccd46a7fd97407
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2014-07-23 04:25:15 +0400

    fix issues that using lists

--- test/battle_solutions/flatten_test.clj
+++ test/battle_solutions/flatten_test.clj
@@ -3,7 +3,7 @@
             [battle-asserts.test-helper :refer [assert-equal]]))
 
 (deftest test-asserts
-  (assert-equal [1 2 {:a 'b'}] (flatten [1 [2 [{:a 'b'}]]]))
+  (assert-equal [1 2 {:a "b"}] (flatten [1 [2 [{:a "b"}]]]))
   (assert-equal [1 2 3 4 5 6 7 8] (flatten [1 [2 3 [4 5 [6 7]]] 8]))
   (assert-equal [] (flatten []))
   (assert-equal [1 2 3 4] (flatten [[1 2] [3 4]])))
