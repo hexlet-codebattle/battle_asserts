ab6beaa35f93cefa0bf524c2a1216d9a756d8f22
Krotos <krotos2112@gmail.com>
2014-07-24 15:22:48 +0400

    Added the-big-divide

---
+++ test/battle_solutions/the_big_divide_test.clj
@@ -0,0 +1,15 @@
+(ns battle-solutions.the-big-divide-test
+  (:require [clojure.test :refer :all]
+            [battle-asserts.test-helper :refer [assert-equal]]))
+
+(defn the-big-divide
+  [supr num1 num2]
+  (reduce + (distinct (concat (range num1 supr num1)
+                              (range num2 supr num2)))))
+
+
+(deftest test-asserts
+  (assert-equal 0 (the-big-divide 3 17 11))
+  (assert-equal 23 (the-big-divide 10 3 5))
+  (assert-equal 233168 (the-big-divide 1000 3 5)))
+
