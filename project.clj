fc7534758e09c45b9bd6372b8b0e2f059d99a227
Kirill Mokevnin <mokevnin@gmail.com>
2015-08-23 16:48:02 -0400

    change required lein version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :min-lein-version "2.5.2"
+  :min-lein-version "2.5.1"
   :plugins [[lein-kibit "0.1.2"]]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.namespace "0.2.11"]
