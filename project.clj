cb2c8930fc328ec358233584b43e8f13d1f3c8a2
Kirill Mokevnin <mokevnin@gmail.com>
2015-08-23 17:05:42 -0400

    revert clojure version

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.5.1"
   :plugins [[lein-kibit "0.1.2"]]
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.namespace "0.2.11"]
                  [me.raynes/fs "1.4.6"]
                  [circleci/clj-yaml "0.5.4"]
