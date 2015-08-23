ea9154c3ae78b6b50b2c1ff3d2fa38f734d6f885
Kirill Mokevnin <mokevnin@gmail.com>
2015-08-23 16:20:51 -0400

    update deps versions

--- project.clj
+++ project.clj
@@ -3,19 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :min-lein-version "2.3.0"
-  :plugins [[lein-kibit "0.0.8"]]
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.namespace "0.2.4"]
-                 [me.raynes/fs "1.4.4"]
-                 [circleci/clj-yaml "0.5.2"]
-                 [multicode "0.1.0-SNAPSHOT"]
-                 [clj-stacktrace "0.2.7"]]
-  :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
-                                      'print-cause-trace)
-                     new (ns-resolve (doto 'clj-stacktrace.repl require)
-                                     'pst)]
-                 (alter-var-root orig (constantly (deref new))))]
+  :min-lein-version "2.5.2"
+  :plugins [[lein-kibit "0.1.2"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.namespace "0.2.11"]
+                 [me.raynes/fs "1.4.6"]
+                 [circleci/clj-yaml "0.5.4"]
+                 [multicode "0.1.0-SNAPSHOT"]]
   :source-paths ["src"]
   :eval-in :nrepl
   :main battle-asserts.core)
