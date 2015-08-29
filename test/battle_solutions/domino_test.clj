6c7e7736190d0731a2dee7b3d28fd625f9b7bcab
Gregory Romantsov <imoonserk@gmail.com>
2015-08-29 15:15:44 +0400

    Add domino issue

---
+++ test/battle_solutions/domino_test.clj
@@ -0,0 +1,17 @@
+(ns battle-solutions.domino-test
+  (:require [clojure.test :refer :all]
+            [battle-asserts.test-helper :refer [assert-equal]]))
+
+(defn domino [x]
+  (letfn [(rat [x]
+            (+ (reduce + (range (+ x 1)))
+               (* x (+ x 1))))]
+    (->> (range (+ x 1))
+         (map #(rat %))
+         (reduce +))))
+
+(deftest test-asserts
+  (assert-equal 0  (domino 0))
+  (assert-equal 3  (domino 1))
+  (assert-equal 12 (domino 2))
+  (assert-equal 30 (domino 3)))
