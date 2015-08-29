dff5417dbfd0da2e7eaf555aa57211d692bebe71
Kirill Mokevnin <mokevnin@gmail.com>
2015-08-29 10:04:02 -0400

    add plugins

--- Makefile
+++ Makefile
@@ -1,8 +1,19 @@
-generate:
+generate: format
 	lein run
+
+format:
+	lein cljfmt fix
+
+checks: check-format check-namespaces check-style
+
+check-format:
+	lein cljfmt check
 
 check-style:
 	lein kibit
+
+check-namespaces:
+	lein eastwood
 
 test:
 	lein trampoline test
--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.5.1"
-  :plugins [[lein-kibit "0.1.2"]]
+  :plugins [[lein-kibit "0.1.2"]
+            [lein-cljfmt "0.3.0"]
+            [jonase/eastwood "0.2.1"]]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.namespace "0.2.11"]
                  [me.raynes/fs "1.4.6"]
