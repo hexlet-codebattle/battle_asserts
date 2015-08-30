b0e13f1b0c7ee01db44fa5d85fec54549373e7b0
Gregory Romantsov <imoonserk@gmail.com>
2015-08-30 16:48:09 +0400

    Fixed on letfn

--- test/battle_solutions/domino_test.clj
+++ test/battle_solutions/domino_test.clj
@@ -3,18 +3,19 @@
             [battle-asserts.test-helper :refer [assert-equal]]))
 
 (defn domino [x]
-  (defn rat [x]
-    (defn nu [x] 
-      (if (= x 0)
-        x
-        (+ x (nu (- x 1)))))
-    (defn de [x] (* x (+ x 1)))
-    (+ (nu x)(de x)))
-  (defn sum [it, acc]
-    (if (= it 0)
-      acc
-      (sum (- it 1)(+ acc (rat it)))))
-  (sum x 0))   
+  (letfn [(rat [x]
+            (letfn [(nu [x]
+                      (if (= x 0)
+                        x
+                        (+ x (nu (- x 1)))))
+                    (de [x]
+                        (* x (+ x 1)))]
+              (+ (nu x) (de x))))
+          (sum [it acc]
+               (if (= it 0)
+                 acc
+                 (sum (- it 1) (+ acc (rat it)))))]
+    (sum x 0)))
 
 (deftest test-asserts
   (assert-equal 0  (domino 0))
