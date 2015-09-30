aecf4ec4ecab0d47f17a6209fc706c66fdef9422
Kirill Mokevnin <mokevnin@gmail.com>
2015-09-30 17:18:26 -0400

    fix issue

--- test/battle_solutions/drop_every_n_element_test.clj
+++ test/battle_solutions/drop_every_n_element_test.clj
@@ -8,7 +8,7 @@
                 coll))
 
 (deftest test-asserts
-  (assert-equal '(a b d e g h k) (drop-every 3 '(a b c d e f g h i k)))
+  (assert-equal '("a" "b" "d" "e" "g" "h" "k") (drop-every 3 '("a" "b" "c" "d" "e" "f" "g" "h" "i" "k")))
   (assert-equal [0 2 4 6 8] (drop-every 2 [0 1 2 3 4 5 6 7 8 9]))
   (assert-equal [0 1 2 3] (drop-every 5 [0 1 2 3]))
   (assert-equal [] (drop-every 1 [0 1 2 3 4 5 6 7])))
