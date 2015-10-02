583606a21ba8b2daed0e4ef7b27f3f671bbd2b63
Roman Dementev <roman.dementev@hexlet-source.com>
2015-10-02 18:44:51 +0300

    add move zeros problem

---
+++ test/battle_solutions/move_zeros_test.clj
@@ -0,0 +1,13 @@
+(ns battle-solutions.move-zeros-test
+  (:require [clojure.test :refer :all]
+            [battle-asserts.test-helper :refer :all]))
+
+(defn move-zeros-test [array]
+  (concat
+   (filter #(not (zero? %)) array)
+   (filter zero? array)))
+
+(deftest test-asserts
+  (assert-equal [1 3 12 0 0] (move-zeros-test [0 1 0 3 12]))
+  (assert-equal [4 3 5 0 0 0] (move-zeros-test [4 0 3 0 5 0]))
+  (assert-equal [4 3 5 0 0 0] (move-zeros-test [0 0 0 4 3 5])))
