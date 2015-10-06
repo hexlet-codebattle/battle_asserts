22f7de0be46fbba4d0c0cdbd7d7f557abfdc61e4
Ilya Shakirov <aelaau@gmail.com>
2015-10-06 16:36:44 +0300

    Add tests

--- test/battle_solutions/base_n_test.clj
+++ test/battle_solutions/base_n_test.clj
@@ -4,13 +4,16 @@
 
 (defn is-base-n
   [number base]
-  (-> base
-      str
+  (-> (str "["
+           (->> (range base 10)
+                (reduce str))
+           "]")
       re-pattern
       (re-find (str number))
       nil?))
 
 (deftest test-asserts
+  (assert-equal false (is-base-n 6161 3))
+  (assert-equal true (is-base-n 1010 2))
   (assert-equal false (is-base-n 2341 4))
-  (assert-equal true (is-base-n 5511 8))
-  )
+  (assert-equal true (is-base-n 5511 8)))
