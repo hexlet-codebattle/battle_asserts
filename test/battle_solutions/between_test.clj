9b01478b66c67e10e7361bb7f6d272bbad79322f
Ilya Shakirov <aelaau@gmail.com>
2015-10-07 16:45:29 +0300

    Change problem level

--- test/battle_solutions/between_test.clj
+++ test/battle_solutions/between_test.clj
@@ -12,4 +12,13 @@
 (deftest test-asserts
   (let [str1 "<a>foo</a>"
         str2 ["foo"]]
-    (assert-equal str2 (between str1 "<a>" "</a>"))))
+    (assert-equal str2 (between str1 "<a>" "</a>")))
+  (let [str1 "this is 'text'"
+        str2 ["text"]]
+    (assert-equal str2 (between str1 "'" "'")))
+  (let [str1 "www.google.com"
+        str2 ["google"]]
+    (assert-equal str2 (between str1 "www." ".com")))
+  (let [str1 "xonix"
+        str2 ["oni"]]
+    (assert-equal str2 (between str1 "x" "x"))))
