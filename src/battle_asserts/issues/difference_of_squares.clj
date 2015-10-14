f29c7f6bc702c704205b120c7a176ecefef1b007
Roman Dementev <roman.dementev@hexlet-source.com>
2015-10-14 15:47:27 +0300

    converted difference of squares

---
+++ src/battle_asserts/issues/difference_of_squares.clj
@@ -0,0 +1,27 @@
+(ns battle-asserts.issues.difference-of-squares
+  (:require [clojure.test.check.generators :as gen]
+            [faker.generate :as faker]))
+
+(def level :easy)
+
+(def description "Find the difference between the square of the sums and the sum of the squares of the first N natural numbers.
+                 The square of the sum of the first ten natural numbers is,
+                 (1 + 2 + ... + 10)**2 = 55**2 = 3025
+                 The sum of the squares of the first ten natural numbers is,
+                 1**2 + 2**2 + ... + 10**2 = 385
+                 Hence the difference between the sum of the squares of the first ten
+                 natural numbers and the square of the sum is 3025 - 385 = 2640.")
+
+(defn arguments-generator []
+  (gen/tuple (gen/choose 1 120)))
+
+(defn square-of-sums [value]
+  (let [sum (reduce + (range (inc value)))]
+    (* sum sum)))
+
+(defn sum-of-squares [value]
+  (let [squares (map #(* % %) (range 1 (inc value)))]
+    (reduce + squares)))
+
+(defn solution [value]
+  (- (square-of-sums value) (sum-of-squares value)))
