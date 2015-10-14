dd797e11ee071cf10ff8b9371900b5ff1a05a2d9
Roman Dementev <roman.dementev@hexlet-source.com>
2015-10-14 13:52:26 +0300

    converted arrange number

---
+++ test/battle_asserts/issues/arrange_numbers_to_form_biggest_number_test.clj
@@ -0,0 +1,11 @@
+(ns battle-asserts.issues.arrange-numbers-to-form-biggest-number-test
+  (:require [clojure.test :refer :all]
+            [clojure.test.check.properties :as prop :include-macros true]
+            [clojure.test.check.clojure-test :as ct :include-macros true]
+            [battle-asserts.issues.arrange-numbers-to-form-biggest-number :as issue]))
+
+(deftest test-asserts
+  (is (= 654321 (issue/solution [1 2 3 4 5 6])))
+  (is (= 6054854654 (issue/solution [54 546 548 60])))
+  (is (= 998764543431 (issue/solution [1 34 3 98 9 76 45 4])))
+  (is (= 9908988444332412 (issue/solution [43 44 12 324 90 9 88 89]))))
