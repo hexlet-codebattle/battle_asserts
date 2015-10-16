d8b0fd219c46afe012ef791c100e380b0098ebd9
Roman Dementev <roman.dementev@hexlet-source.com>
2015-10-16 17:08:08 +0300

    convert next lucky number

--- src/battle_asserts/issues/next_lucky_number.clj
+++ src/battle_asserts/issues/next_lucky_number.clj
@@ -1,13 +1,21 @@
-(ns battle-solutions.next-lucky-number-test
-  (:require [clojure.test :refer :all]
-            [battle-asserts.test-helper :refer :all]))
+(ns battle-asserts.issues.next-lucky-number
+  (:require [clojure.test.check.generators :as gen]
+            [faker.generate :as faker]))
+
+(def level :medium)
+
+(def description "Lucky numbers are defined as the numbers consisting only of digits 3 and 5.
+                 So, given a number N, you have to return the least lucky number strictly greater than N.")
+
+(defn arguments-generator []
+  (gen/tuple (gen/choose 0 100)))
 
 (defn prev-digits [prev-digits owerflow]
   (if (zero? owerflow)
     prev-digits
     (repeat (count prev-digits) 3)))
 
-(defn next-lucky-number [number]
+(defn solution [number]
   (Integer.
    (apply str
           (loop [n number, owerflow 0, res '()]
@@ -18,11 +26,3 @@
               (if (and (zero? integer) (zero? cur-overflow))
                 (conj (prev-digits res owerflow) cur-digit)
                 (recur integer cur-overflow (conj res cur-digit))))))))
-
-(deftest test-asserts
-  (assert-equal 5 (next-lucky-number 4))
-  (assert-equal 53 (next-lucky-number 48))
-  (assert-equal 333 (next-lucky-number 130))
-  (assert-equal 333 (next-lucky-number 98))
-  (assert-equal 3333 (next-lucky-number 949))
-  (assert-equal 353 (next-lucky-number 339)))
