8264137cf16d13f159ca57cfc30d5583df7dc461
Rakhim Davletkaliyev <freetonik@gmail.com>
2015-10-20 22:45:27 +0300

    Update more_than_medium.clj

--- src/battle_asserts/issues/more_than_medium.clj
+++ src/battle_asserts/issues/more_than_medium.clj
@@ -4,8 +4,7 @@
 
 (def level :easy)
 
-(def description "Write a function that will return the words,
-                 which are longer than the average length of all the words.")
+(def description "Write a function that returns the words which are longer than the average length of all the words.")
 
 (defn arguments-generator []
   (let [sentences (repeatedly 30 #(faker/sentence {:words-range [1 10]}))]
