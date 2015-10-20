f3f25c8c19017b97bee3a299de1dab61bad6f670
Roman Dementev <roman.dementev@hexlet-source.com>
2015-10-20 16:40:00 +0300

    fix solution next lucky number

--- src/battle_asserts/issues/next_lucky_number.clj
+++ src/battle_asserts/issues/next_lucky_number.clj
@@ -14,6 +14,8 @@
   [{:expected 5
     :arguments [4]}
    {:expected 53
+    :arguments [45]}
+   {:expected 53
     :arguments [48]}
    {:expected 333
     :arguments [130]}
@@ -25,18 +27,19 @@
     :arguments [339]}])
 
 (defn prev-digits [prev-digits owerflow]
-  (if (zero? owerflow)
-    prev-digits
-    (repeat (count prev-digits) 3)))
+  (if owerflow
+    (repeat (count prev-digits) 3)
+    prev-digits))
 
 (defn solution [number]
   (Integer.
    (apply str
-          (loop [n number, owerflow 0, res '()]
+          (loop [n (inc number), owerflow 0, res '()]
             (let [integer (quot n 10)
                   remnant (+ owerflow (rem n 10))
                   cur-digit (if (<= 4 remnant 5) 5 3)
-                  cur-overflow (if (> remnant 5) 1 0)]
+                  cur-overflow (if (> remnant 5) 1 0)
+                  must-overwrite-prev (or (not (zero? owerflow)) (> cur-digit remnant))]
               (if (and (zero? integer) (zero? cur-overflow))
-                (conj (prev-digits res owerflow) cur-digit)
-                (recur integer cur-overflow (conj res cur-digit))))))))
+                (conj (prev-digits res must-overwrite-prev) cur-digit)
+                (recur integer cur-overflow (conj (prev-digits res must-overwrite-prev) cur-digit))))))))
