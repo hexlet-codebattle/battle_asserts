b0454c75cf1b1f17c5bcc37ade940d733cdf9aea
Rakhim Davletkaliyev <freetonik@gmail.com>
2015-10-20 22:39:32 +0300

    Update fixed_point_in_array.clj

--- src/battle_asserts/issues/fixed_point_in_array.clj
+++ src/battle_asserts/issues/fixed_point_in_array.clj
@@ -6,7 +6,7 @@
 
 (def description "Given an array of n distinct integers sorted in ascending order,
                  write a function that returns a Fixed Point in the array,
-                 if there is any Fixed Point present in array, else returns -1.
+                 if there is a Fixed Point present in array; else returns -1.
                  Fixed Point in an array is an index i such that arr[i] is equal to i.")
 
 (defn arguments-generator []
