42c4e996c6f6939cb5228f4b51efe13e02ed6cb3
Rakhim Davletkaliyev <freetonik@gmail.com>
2015-10-20 22:37:01 +0300

    Update excel_sheet_column_number.clj

--- src/battle_asserts/issues/excel_sheet_column_number.clj
+++ src/battle_asserts/issues/excel_sheet_column_number.clj
@@ -4,7 +4,7 @@
 
 (def level :easy)
 
-(def description "Given a column title as appear in an Excel sheet, return its corresponding column number.
+(def description "Given a column title (as it appears in an Excel sheet), return its corresponding column number.
                  For example: A -> 1, B -> 2, C -> 3, ...  Z -> 26, AA -> 27, AB -> 28")
 
 (defn arguments-generator []
