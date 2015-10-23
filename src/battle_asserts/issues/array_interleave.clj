8d87b178c121c8605282748a61a3222600911255
Kirill Mokevnin <mokevnin@gmail.com>
2015-10-23 18:41:50 -0400

    update array interleave

--- src/battle_asserts/issues/array_interleave.clj
+++ src/battle_asserts/issues/array_interleave.clj
@@ -4,18 +4,18 @@
 
 (def level :easy)
 
-(def description "Write a function which interleaves an array with another array. For example, [1 3 5] [2 4 6] -> [1 2 3 4 5 6].")
+(def description "Write a function which interleaves an array with another array.")
 
 (defn arguments-generator []
   (gen/tuple (gen/list gen/int) (gen/list gen/int)))
 
 (def test-data
-  [{:expected [1 2 3 4 5 6]
+  [{:expected [5 "4" 1 "9"]
+    :arguments [[5 1 3] ["4" "9"]]}
+   {:expected [1 2 3 4 5 6]
     :arguments [[1 3 5] [2 4 6]]}
    {:expected [1 2 3 4]
     :arguments [[1 3 5] [2 4]]}
-   {:expected [1 "2" 3 "4"]
-    :arguments [[1 3 5] ["2" "4"]]}
    {:expected []
     :arguments [[] [2 4 6]]}])
 
