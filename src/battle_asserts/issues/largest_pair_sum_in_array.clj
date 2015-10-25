5d79102fc243771de4eb79632bc4c9e9e80453c4
Rakhim Davletkaliyev <freetonik@gmail.com>
2015-10-25 22:17:14 +0200

    Update largest_pair_sum_in_array.clj

--- src/battle_asserts/issues/largest_pair_sum_in_array.clj
+++ src/battle_asserts/issues/largest_pair_sum_in_array.clj
@@ -5,7 +5,7 @@
 (def level :easy)
 
 (def description "Given an array of integers, find the largest pair sum in it. For example,
-                 the largest pair sum in [ 12, 34, 10, 6, 40 ] is 74.")
+                 the largest pair sum in [12, 34, 10, 6, 40] is 74.")
 
 (defn arguments-generator []
   (gen/tuple (gen/vector gen/int)))
