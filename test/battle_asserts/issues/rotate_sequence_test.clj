f93a8570f073b58d00517d8b1e2fb34e1edf7f82
Roman Dementev <roman.dementev@hexlet-source.com>
2015-10-26 16:55:40 +0300

    converted rotate  sequence

---
+++ test/battle_asserts/issues/rotate_sequence_test.clj
@@ -0,0 +1,15 @@
+(ns battle-asserts.issues.rotate-sequence-test
+  (:require [clojure.test :refer :all]
+            [clojure.test.check.properties :as prop :include-macros true]
+            [clojure.test.check.clojure-test :as ct :include-macros true]
+            [test-helper :as h]
+            [battle-asserts.issues.rotate-sequence :as issue]))
+
+(ct/defspec spec-solution
+  20
+  (prop/for-all [v (issue/arguments-generator)]
+                (=  (sort (apply issue/solution v))
+                   (sort (second v)))))
+
+(deftest test-solution
+  (h/generate-tests issue/test-data issue/solution))
