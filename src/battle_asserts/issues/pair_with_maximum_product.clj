297fd20290c7d3c40bf3e7a89c4c7f8ea3a5d063
Roman Dementev <roman.dementev@hexlet-source.com>
2015-10-27 13:55:23 +0300

    fix solution pair with maximum product

--- src/battle_asserts/issues/pair_with_maximum_product.clj
+++ src/battle_asserts/issues/pair_with_maximum_product.clj
@@ -12,20 +12,27 @@
 (def test-data
   [{:expected [6 7]
     :arguments [[1 4 3 6 7 0]]}
-   {:expected [-5 -4]
+   {:expected [-4 -5]
     :arguments [[-1 -3 -4 2 0 -5]]}
-   {:expected [3 4]
-    :arguments [[-1 -2 -4 -3 0 4 3 2 1]]}])
+   {:expected [-4 -3]
+    :arguments [[-1 -2 -4 -3 0 4 3 2 1]]}
+   {:expected [-3 -3]
+    :arguments [[-3 -3 3 3]]}])
 
 (defn product [array]
   (apply * array))
 
+(defn make-pair [first-array second-array]
+  (map vector
+       first-array
+       second-array))
+
 (defn solution [array]
-  (let [sort-array (sort array)
-        two-max (take-last 2 sort-array)
-        two-min (take 2 sort-array)]
-    (if (>
-         (product two-min)
-         (product two-max))
-      two-min
-      two-max)))
+  (->>
+   array
+   (reduce-kv #(let [next-elements (drop (inc %2) array)]
+                 (concat %1 (make-pair (cycle [%3])
+                                       next-elements)))
+              [])
+   reverse
+   (apply max-key product)))
