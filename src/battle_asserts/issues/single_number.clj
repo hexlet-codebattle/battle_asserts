6de3e1467e04e5a52635f0b7e7cd940c821c4f4e
Roman Dementev <roman.dementev@hexlet-source.com>
2015-10-28 18:41:45 +0300

    converted single number

---
+++ src/battle_asserts/issues/single_number.clj
@@ -0,0 +1,30 @@
+(ns battle-asserts.issues.single-number
+  (:require [clojure.test.check.generators :as gen]
+            [faker.generate :as faker]))
+
+(def level :easy)
+
+(def description "Given an array of integers, every element appears twice except for one. Find that single one.")
+
+(defn arguments-generator []
+  (letfn [(modify [array]
+            (let [unique-elements (set array)
+                  single-element (last unique-elements)
+                  without-last (drop-last unique-elements)]
+              (gen/shuffle (apply conj without-last single-element without-last))))]
+    (gen/tuple (gen/bind (gen/such-that #(> (count (set %)) 3)
+                                        (gen/vector gen/int 3 25))
+                         modify))))
+
+(def test-data
+  [{:expected 6
+    :arguments [[8 6 8 7 2 2 7 1 9 9 1]]}
+   {:expected 3
+    :arguments [[2 2 3]]}
+   {:expected 4
+    :arguments [[4 2 2 3 6 3 8 7 8 6 7]]}
+   {:expected 5
+    :arguments [[1 3 4 3 5 6 7 1 6 7 4]]}])
+
+(defn solution [array]
+  (apply bit-xor array))
