c022d4f301aae5ef7da35a0c0f6c1d47a30e3159
Kirill Mokevnin <mokevnin@gmail.com>
2015-10-28 11:02:44 -0400

    fix biggest number test

--- test/battle_asserts/issues/arrange_numbers_to_form_biggest_number_test.clj
+++ test/battle_asserts/issues/arrange_numbers_to_form_biggest_number_test.clj
@@ -9,7 +9,7 @@
 (ct/defspec spec-solution
   20
   (prop/for-all [v (issue/arguments-generator)]
-                (>= (apply issue/solution v)
+                (>= (BigInteger. (apply issue/solution v))
                     (BigInteger. (s/join (first v))))))
 
 (deftest test-solution
