df390ce74d167a345a72b50b63e23a9be33c8183
Roman Dementev <roman.dementev@hexlet-source.com>
2015-10-29 16:28:18 +0300

    converted_supremum_syracuse

---
+++ src/battle_asserts/issues/supremum_syracuse.clj
@@ -0,0 +1,29 @@
+(ns battle-asserts.issues.supremum-syracuse
+  (:require [clojure.test.check.generators :as gen]
+            [faker.generate :as faker]))
+
+(def level :medium)
+
+(def description "Find the largest element in the Syracuse sequence.")
+
+(defn arguments-generator []
+  (gen/tuple (gen/choose 1 100)))
+
+(def test-data
+  [{:expected 16
+    :arguments [6]}
+   {:expected 52
+    :arguments [11]}
+   {:expected 9232
+    :arguments [27]}])
+
+(defn solution [num]
+  (letfn [(gen-seq
+            [num]
+            (if (= 1 num)
+              [num]
+              (concat [num]
+                      (if (odd? num)
+                        (gen-seq (inc (* 3 num)))
+                        (gen-seq (/ num 2))))))]
+    (apply max (gen-seq num))))
