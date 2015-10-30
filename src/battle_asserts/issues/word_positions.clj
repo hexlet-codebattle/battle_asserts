2f6ce526fc9b929730d69fef207316689a6b5181
Roman Dementev <roman.dementev@hexlet-source.com>
2015-10-30 12:27:47 +0300

    converted word position

---
+++ src/battle_asserts/issues/word_positions.clj
@@ -0,0 +1,31 @@
+(ns battle-asserts.issues.word-positions
+  (:require [clojure.test.check.generators :as gen]
+            [faker.generate :as faker]))
+
+(def level :elementary)
+
+(def description "Given a text file and a word, find the positions that the word occurs in the file.
+                 We’ll be asked to find the positions of many words in the same file.")
+
+(defn arguments-generator []
+  (letfn [(input []
+            (let [sentence (faker/sentence {:words-range [1, 10]})
+                  words (re-seq #"\w+" sentence)]
+              [sentence, (rand-nth words)]))]
+    (gen/elements (repeatedly 50 input))))
+
+(def test-data
+  [{:expected [12]
+    :arguments ["find a word in some sentence" "in"]}
+   {:expected [0]
+    :arguments ["test" "test"]}
+   {:expected [0 5 10]
+    :arguments ["test test test" "test"]}])
+
+(defn solution [string word]
+  (letfn [(step [x]
+            (when (.find x)
+              (cons (.start x) (lazy-seq (step x)))))]
+    (let [m (re-matcher (re-pattern (str "\\b" word "\\b"))
+                        string)]
+      (vec (step m)))))
