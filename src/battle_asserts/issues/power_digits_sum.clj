718557b9f1d87fbbda58e23d78b3429a9f903423
Kirill Mokevnin <mokevnin@gmail.com>
2015-11-02 18:40:54 +0300

    escape markdown in power digits sum

--- src/battle_asserts/issues/power_digits_sum.clj
+++ src/battle_asserts/issues/power_digits_sum.clj
@@ -4,8 +4,8 @@
 
 (def level :easy)
 
-(def description "Calculate the sum of digits of 2**n
-                 For example, 2**15 = 32768, and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.")
+(def description "Calculate the sum of digits of 2\\*\\*n
+                 For example, 2\\*\\*15 = 32768, and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.")
 
 (defn arguments-generator []
   (gen/tuple (gen/choose 0 30)))
