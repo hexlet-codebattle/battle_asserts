6999e8d4a0c2a62b09628500ae2cbd35f11d04de
Roman Dementev <roman.dementev@hexlet-source.com>
2015-11-02 12:40:11 +0300

    converted read roman numerals

---
+++ src/battle_asserts/issues/read_roman_numerals.clj
@@ -0,0 +1,32 @@
+(ns battle-asserts.issues.read-roman-numerals
+  (:require [clojure.test.check.generators :as gen]
+            [clojure.pprint :as pprint]
+            [faker.generate :as faker]))
+
+(def level :hard)
+
+(def description "Write a function to parse a Roman-numeral string and return the number it represents.")
+
+(defn arguments-generator []
+  (letfn [(to-roman [number]
+            (pprint/cl-format nil "~@R" number))]
+    (gen/tuple (gen/bind (gen/choose 1 3500) #(gen/return (to-roman %))))))
+
+(def test-data
+  [{:expected 14
+    :arguments ["XIV"]}
+   {:expected 827
+    :arguments ["DCCCXXVII"]}
+   {:expected 3999
+    :arguments ["MMMCMXCIX"]}
+   {:expected 48
+    :arguments ["XLVIII"]}])
+
+(defn solution [roman]
+  (let [numerals {\I 1, \V 5, \X 10, \L 50, \C 100, \D 500, \M 1000}]
+    (loop [roman-reverse (clojure.string/reverse roman) last-element 0 sum 0]
+      (if (empty? roman-reverse)
+        sum
+        (let [value (numerals (first roman-reverse))
+              real-value (if (> last-element value) (* -1 value) value)]
+          (recur (subs roman-reverse 1) value (+ sum real-value)))))))
