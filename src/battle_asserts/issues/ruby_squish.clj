17595ad5497f65e89192a2553a2a0a3b96199b6e
Manin Ilya <bkmzvf@gmail.com>
2015-11-06 11:51:21 +0600

    Added ruby-like squish function

---
+++ src/battle_asserts/issues/ruby_squish.clj
@@ -0,0 +1,39 @@
+(ns battle-asserts.issues.ruby-squish
+  (:require [clojure.string :as string]
+            [clojure.test.check.generators :as gen]
+            [faker.generate :as faker]))
+
+(def level :elementary)
+
+(def description "Returns the string, first removing all whitespace on both ends
+                 of the string, and then changing remaining consecutive whitespace
+                 groups into one space each. Note that it handles both ASCII and
+                 Unicode whitespace.")
+
+(defn separators []
+  (apply str (repeat (rand-int 10) (rand-nth ["\t" "\n" " "]))))
+
+(defn arguments-generator []
+  (gen/tuple
+    (gen/elements
+      (repeatedly
+        20
+        #(string/join
+          (separators)
+          (conj
+            (into
+              [(separators)]
+              (string/split
+                (faker/sentence {:words-range [1,5] :lang :ru})
+                #"\s"))
+            " "))))))
+
+(def test-data
+  [{:expected "Multi-line string is so cool"
+    :arguments ["  Multi-line \n       string    \t   is   \n   so cool   \n"]}])
+
+(defn squeeze_spaces [str]
+  (string/replace str #"\s+" " "))
+
+(defn solution [str]
+ (-> str string/triml string/trimr squeeze_spaces))
