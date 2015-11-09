b45f30b3ec363870d4e13b7b295620f0eaa3c732
Roman Dementev <roman.dementev@hexlet-source.com>
2015-11-09 13:34:04 +0300

    add sublist

---
+++ src/battle_asserts/issues/sublist.clj
@@ -0,0 +1,44 @@
+(ns battle-asserts.issues.sublist
+  (:require [clojure.test.check.generators :as gen]
+            [clojure.string :as s]
+            [faker.generate :as faker]))
+
+(def level :easy)
+
+(def description "Write a function that given two lists determines if the first list is
+                 contained within the second list, if the second list is contained within
+                 the first list, if both lists are contained within each other or if none
+                 of these are true.
+                 A = [3, 4], B = [1, 2, 3, 4, 5], A is sublist of B
+                 A = [1, 2, 3], B = [1, 2, 3], A is equal B
+                 A = [1, 2, 3, 4, 5], B = [2, 3, 4], A is superlist of B
+                 A = [1, 2, 4], B = [1, 2, 3, 4, 5], A is unequal B]")
+
+(defn arguments-generator []
+  (letfn [(superlist [a]
+            (rand-nth (partition (rand-int (count a)) 1 a)))
+          (sublist [a]
+                   (concat (repeatedly (rand-int 4) #(rand-int 10))
+                           a
+                           (repeatedly (rand-int 4) #(rand-int 10))))]
+    (gen/bind (gen/vector gen/int 4 8)
+              #(gen/tuple (gen/return %) (gen/one-of [(gen/return (superlist %))
+                                                      (gen/return (sublist %))
+                                                      (gen/return %)
+                                                      (gen/vector gen/int 4 8)])))))
+
+(def test-data
+  [{:arguments [[1 2 3] [1 2 3 4 5]] :expected "A is sublist of B"}
+   {:arguments [[3 4 5] [1 2 3 4 5]] :expected "A is sublist of B"}
+   {:arguments [[3 4] [1 2 3 4 5]] :expected "A is sublist of B"}
+   {:arguments [[1 2 3] [1 2 3]] :expected "A is equal B"}
+   {:arguments [[1 2 3 4 5] [2 3 4]] :expected "A is superlist of B"}
+   {:arguments [[1 2 4] [1 2 3 4 5]] :expected "A is unequal B"}])
+
+(defn solution [a b]
+  (cond
+    (= a b) "A is equal B"
+    (some #{a} (partition (count a) 1 b)) "A is sublist of B"
+    (some #{b} (partition (count b) 1 a)) "A is superlist of B"
+    :else "A is unequal B"))
+
