1ddc5849ce6b8ccdaba87ce1c977b9c1315cdb4b
Manin Ilya <bkmzvf@gmail.com>
2015-11-09 17:30:40 +0600

    RF made more clojure style

--- src/battle_asserts/issues/ruby_squish.clj
+++ src/battle_asserts/issues/ruby_squish.clj
@@ -10,16 +10,20 @@
                  groups into one space each. Note that it handles both ASCII and
                  Unicode whitespace.")
 
-(defn separators []
-  (repeatedly (rand-int 10)
-              #(rand-nth ["\t" "\n" " "])))
+(defn- random-separator []
+  (rand-nth ["\t" "\n" " "]))
+
+(defn- random-separators [n]
+  (string/join (repeatedly n random-separator)))
+
+(defn- prepared-string []
+  (->>
+    (faker/words {:lang :en :n 10})
+    (map #(str (random-separators 5) % (random-separators 5)))
+    (string/join)))
 
 (defn arguments-generator []
-  (gen/tuple
-   (gen/elements
-    (repeatedly 20 #(string/join (interleave (separators)
-                                             (string/split (faker/sentence {:lang :en :words-range [1,5]}) #"\s")
-                                             (separators)))))))
+  (gen/tuple (gen/elements (repeatedly 20 prepared-string))))
 
 (def test-data
   [{:expected "Multi-line string is so cool"
