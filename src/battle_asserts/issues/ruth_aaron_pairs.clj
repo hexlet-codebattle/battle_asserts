1e188cf21d85242ad9cbfe21530c69b555ee6343
Roman Dementev <roman.dementev@hexlet-source.com>
2015-11-11 12:39:40 +0300

    add ruth aaron pair problem

---
+++ src/battle_asserts/issues/ruth_aaron_pairs.clj
@@ -0,0 +1,48 @@
+(ns battle-asserts.issues.ruth-aaron-pairs
+  (:require [clojure.string :as string]
+            [clojure.test.check.generators :as gen]
+            [faker.generate :as faker]))
+
+(def level :easy)
+
+(def description "#FIXME Написать функцию проверяющую является ли пара чисел парой Рута-Ааорона.
+                 Пара Рута-Ааорона состоит из двух последовательных чисел (например 714 715), суммы
+                 простоых множителей которых равны. При нахождении суммы простых множителей
+                 необходимо учитывать повторения множителей")
+
+(def ruth-aaron-pairs [[5 6] [8 9] [15 16] [77 78] [125 126] [714 715] [948 949] [1330 1331] [1520 1521]
+                       [1862 1863] [2491 2492] [3248 3249] [4185 4186] [4191 4192] [5405 5406] [5560 5561]
+                       [5959 5960] [6867 6868] [8280 8281] [8463 8464] [10647 10648] [12351 12352] [14587 14588]
+                       [16932 16933] [17080 17081] [18490 18491] [20450 20451] [24895 24896] [26642 26643] [26649 26650]])
+
+(defn arguments-generator []
+  (gen/tuple (gen/one-of [(gen/elements ruth-aaron-pairs)
+                          (gen/bind gen/pos-int #(gen/return [%, (inc %)]))])))
+
+(def test-data
+  [{:expected true
+    :arguments [[8 9]]}
+   {:expected true
+    :arguments [[5 6]]}
+   {:expected true
+    :arguments [[77 78]]}
+   {:expected false
+    :arguments [[20 21]]}])
+
+(defn- factorization [value [first-prime & rest-primes :as primes]]
+  (cond
+    (> 2 value) '()
+    (zero? (mod value first-prime)) (cons first-prime (lazy-seq (factorization (quot value first-prime) primes)))
+    :else (recur value rest-primes)))
+
+(defn- primes-up-to [value]
+  (take-while #(>= value %)
+              (iterate #(.nextProbablePrime (biginteger %)) 2)))
+
+(defn prime-factorization [value]
+  (factorization value (primes-up-to value)))
+
+(defn solution [[value1 value2]]
+  (and (= (inc value1) value2)
+       (= (apply + (prime-factorization value1))
+          (apply + (prime-factorization value2)))))
