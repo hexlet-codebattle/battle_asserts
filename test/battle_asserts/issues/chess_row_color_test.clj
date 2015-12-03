4de5ea5909a5b382b9b8e181f09b85d754cc5c3f
Roman Dementev <roman.dementev@hexlet-source.com>
2015-12-03 12:23:32 +0300

    change return values for ches_row_color issue

--- test/battle_asserts/issues/chess_row_color_test.clj
+++ test/battle_asserts/issues/chess_row_color_test.clj
@@ -8,7 +8,7 @@
 (ct/defspec spec-solution
   20
   (prop/for-all [v (issue/arguments-generator)]
-                (instance? Boolean (apply issue/solution v))))
+                (string? (apply issue/solution v))))
 
 (deftest test-solution
   (h/generate-tests issue/test-data issue/solution))
