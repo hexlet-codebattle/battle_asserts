cc0419ad500ae618ebefc8da391ea8e24dd73fbc
Rakhim Davletkaliyev <freetonik@gmail.com>
2015-12-23 18:21:40 +0200

    Update difference_of_squares.clj

--- src/battle_asserts/issues/difference_of_squares.clj
+++ src/battle_asserts/issues/difference_of_squares.clj
@@ -6,9 +6,9 @@
 
 (def description "Find the difference between the square of the sum and the sum of the squares of the first N natural numbers.
                  The square of the sum of the first ten natural numbers is,
-                 (1 + 2 + ... + 10)**2 = 55**2 = 3025
+                 (1 + 2 + ... + 10)^2 = 55^2 = 3025
                  The sum of the squares of the first ten natural numbers is,
-                 1**2 + 2**2 + ... + 10**2 = 385
+                 1^2 + 2^2 + ... + 10^2 = 385
                  Hence, the difference between the sum of the squares of the first ten
                  natural numbers and the square of the sum is 3025 - 385 = 2640.")
 
