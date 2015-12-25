352685a18c16716da5fbe36bb233704ef3fa0366
V.Kolesnikov <re.vkolesnikov@gmail.com>
2015-12-26 01:28:34 +0300

    Add rect area problem

---
+++ src/battle_asserts/issues/rect_area.clj
@@ -0,0 +1,20 @@
+(ns battle-asserts.issues.rect-area
+  (:require [clojure.test.check.generators :as gen]))
+
+(def level :elementary)
+
+(def description "Calculate the area of a rectangle given its length and width")
+
+(defn arguments-generator []
+  (gen/tuple gen/pos-int gen/pos-int))
+
+(def test-data
+  [{:expected 0 :arguments [0 0]}
+   {:expected 2 :arguments [1 2]}
+   {:expected 9 :arguments [3 3]}
+   {:expected 4096 :arguments [64 64]}
+   {:expected 13.5 :arguments [1.5 9]}])
+
+(defn solution
+  [width height]
+  (* width height))
