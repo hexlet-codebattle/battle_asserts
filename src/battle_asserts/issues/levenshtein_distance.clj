3af7713af4d766092fcac2c93bea66aeb4e86142
Dmitriy Startsev <Dmitriy Startsev>
2016-02-29 00:17:26 +0300

    Replace Levenshtein Distance algorithm with faster one

--- src/battle_asserts/issues/levenshtein_distance.clj
+++ src/battle_asserts/issues/levenshtein_distance.clj
@@ -40,11 +40,21 @@
     :arguments [[1 2 3 4] [0 2 3 4 5]]}])
 
 (defn solution [seq1 seq2]
-  (cond
-    (empty? seq1) (count seq2)
-    (empty? seq2) (count seq1)
-    :else (min
-           (+ (if (= (first seq1) (first seq2)) 0 1)
-              (#'solution (rest seq1) (rest seq2)))
-           (inc (#'solution (rest seq1) seq2))
-           (inc (#'solution seq1 (rest seq2))))))
+  (letfn [(cell-value [same-char? prev-row cur-row col-idx]
+            (min (inc (nth prev-row col-idx))
+                 (inc (last cur-row))
+                 (+ (nth prev-row (dec col-idx)) (if same-char? 0 1))))]
+    (loop [row-idx 1
+           max-rows (inc (count seq2))
+           prev-row (range (inc (count seq1)))]
+      (if (= row-idx max-rows)
+        (last prev-row)
+        (let [ch2 (nth seq2 (dec row-idx))
+              next-prev-row (reduce (fn [cur-row i]
+                                      (let [same-char? (= (nth seq1 (dec i)) ch2)]
+                                        (conj cur-row (cell-value same-char?
+                                                                  prev-row
+                                                                  cur-row
+                                                                  i))))
+                                    [row-idx] (range 1 (count prev-row)))]
+          (recur (inc row-idx) max-rows next-prev-row))))))
