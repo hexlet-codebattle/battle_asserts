e481ae318651c87b3fc2a17d8b58d3e7547cd4a8
vtm9 <vtmilyakov@yandex.ru>
2019-07-27 01:07:27 +0300

    Improve examples

--- src/battle_asserts/core.clj
+++ src/battle_asserts/core.clj
@@ -25,15 +25,10 @@
         description @(ns-resolve issue-ns-name 'description)
         sample (first @(ns-resolve issue-ns-name 'test-data))]
     (let [filename (str "issues/" issue-name ".yml")
-          json-options [:escape-unicode false :escape-slash false]
-          arguments (s/join ", " (map #(apply json/write-str % json-options) (:arguments sample)))
-          expected (apply json/write-str (:expected sample) json-options)
           metadata {:level @(ns-resolve issue-ns-name 'level)
                     :disabled (if disabled (disabled) false)
                     :signature (if signature (signature) {})
-                    :description (str description
-                                      "\n\n"
-                                      "Example: `" expected " == solution(" arguments ")`")}
+                    :description (render_description description, samples)
           yaml (yaml/generate-string metadata :dumper-options {:flow-style :block})]
       (spit filename yaml))
 
@@ -42,6 +37,27 @@
       (with-open [w (io/writer filename)]
         (doall (map #(.write w (str (json/write-str %) "\n"))
                     asserts))))))
+
+(defn render_examples [description, samples]
+  json-options [:escape-unicode false :escape-slash false]
+  (let [
+        json-options [:escape-unicode false :escape-slash false]
+        to-json #(json/write-str % json-options)
+        array-to-string #(s ", " (map to-json % ))
+        samples-string (->
+                          samples
+                          (mapv #(str
+                                  (to-json (:expected %))
+                                  " == solution("
+                                  array-to-string(:arguments %)
+                                  ")"
+                                  )
+                                )
+                          )
+        ]
+    (str description "\n\n**Example:**`" (s/join "\n" samples-string))
+    )
+  )
 
 (defn -main [& args]
   (let [namespaces (nsf/find-namespaces-in-dir (clojure.java.io/as-file "src/battle_asserts/issues"))]
