56f5c41e59affe2086da1d11160d75f81e2dcba8
solar05 <artem00298@gmail.com>
2020-07-04 23:50:38 +0300

    Add leap year task

---
+++ src/battle_asserts/issues/leap_year.clj
@@ -0,0 +1,33 @@
+(ns battle-asserts.issues.leap-year
+  (:require [clojure.string :as s]
+            [clojure.test.check.generators :as gen]
+            [faker.generate :as faker]))
+
+(def level :elementary)
+
+(def description "Determine the leap year.
+                  Leap years are all divisible by 4,
+                  with the exception of centuries, which are not divisible by 400.")
+
+(def signature
+  {:input [{:argument-name "year" :type {:name "integer"}}]
+   :output {:type {:name "integer"}}})
+
+(defn arguments-generator
+  []
+  (gen/tuple (gen/choose 0 3000)))
+
+(def test-data
+  [{:expected true
+    :arguments [2012]}
+   {:expected false
+    :arguments [1913]}
+   {:expected true
+    :arguments [1804]}
+   {:expected false
+    :arguments [-1913]}
+   {:expected true
+    :arguments [2020]}])
+
+(defn solution [year]
+  (if (and (zero? (mod year 4)) (not (and (zero? (mod year 100)) (not= (mod year 400) 0)))) true false))
