4546ec0bf01cdcf32f183e4cff1cfb009657cd09
solar05 <artem00298@gmail.com>
2020-07-09 20:51:54 +0300

    Refactored solution

--- src/battle_asserts/issues/complex_number_mul.clj
+++ src/battle_asserts/issues/complex_number_mul.clj
@@ -25,7 +25,7 @@
     :arguments [[2 3], [-1 1]]}])
 
 (defn solution [[real1 img1] [real2 img2]]
-  (letfn [(real-part [pair] (- (* (first (first pair)) (first (last pair))) (* (last (first pair)) (last (last pair)))))
-          (imaginary-part [pair] (+ (* (first (last pair)) (last (first pair))) (* (first (first pair)) (last (last pair)))))
+  (letfn [(real-part [first-pair second-pair] (- (* (first first-pair) (first second-pair)) (* (last first-pair) (last second-pair))))
+          (imaginary-part [first-pair second-pair] (+ (* (first first-pair) (last second-pair)) (* (last first-pair) (first second-pair))))
           (format-imaginary [num] (if (>= num 0) (str "+" num) num))]
-    (str (real-part [[real1 img1] [real2 img2]]) (format-imaginary (imaginary-part [[real1 img1] [real2 img2]])) "i")))
+    (str (real-part [real1 img1] [real2 img2]) (format-imaginary (imaginary-part [real1 img1] [real2 img2])) "i")))
