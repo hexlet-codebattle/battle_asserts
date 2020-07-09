4fcc8a1521fca92812687ab6e4d44b16847506eb
solar05 <artem00298@gmail.com>
2020-07-09 20:37:06 +0300

    Add complex num mul task

---
+++ src/battle_asserts/issues/complex_number_mul.clj
@@ -0,0 +1,31 @@
+(ns battle-asserts.issues.complex-number-mul
+  (:require [clojure.test.check.generators :as gen]))
+
+(def level :easy)
+
+(def description "Implement the function that multiply two complex numbers.")
+
+(def signature
+  {:input  [{:argument-name "arr" :type {:name "array" :nested {:name "integer"}}}]
+   :output {:type {:name "string"}}})
+
+(defn arguments-generator
+  []
+  (gen/tuple (gen/tuple gen/small-integer gen/small-integer)
+             (gen/tuple gen/small-integer gen/small-integer)))
+
+(def test-data
+  [{:expected "1-7i"
+    :arguments [[1 -2], [3 -1]]}
+   {:expected "-8+1i"
+    :arguments [[-3 2], [2 1]]}
+   {:expected "4+7i"
+    :arguments [[3 2], [2 1]]}
+   {:expected "-5-1i"
+    :arguments [[2 3], [-1 1]]}])
+
+(defn solution [[real1 img1] [real2 img2]]
+  (letfn [(real-part [pair] (- (* (first (first pair)) (first (last pair))) (* (last (first pair)) (last (last pair)))))
+          (imaginary-part [pair] (+ (* (first (last pair)) (last (first pair))) (* (first (first pair)) (last (last pair)))))
+          (format-imaginary [num] (if (>= num 0) (str "+" num) num))]
+    (str (real-part [[real1 img1] [real2 img2]]) (format-imaginary (imaginary-part [[real1 img1] [real2 img2]])) "i")))
