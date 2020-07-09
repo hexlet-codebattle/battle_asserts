9cd3c261c29e736a82acf4c429c5e8ec836b0e04
solar05 <artem00298@gmail.com>
2020-07-10 02:19:12 +0300

    Add even array square sum task

---
+++ src/battle_asserts/issues/even_array_square_sum.clj
@@ -0,0 +1,31 @@
+(ns battle-asserts.issues.even-array-square-sum
+  (:require [clojure.string :as s]
+            [clojure.test.check.generators :as gen]
+            [faker.generate :as faker]))
+
+(def level :elementary)
+
+(def description "Find square sum of all even numbers in array.")
+
+(def signature
+  {:input [{:argument-name "arr" :type {:name "array" :nested {:name "integer"}}}]
+   :output {:type {:name "integer"}}})
+
+(defn arguments-generator
+  []
+  (gen/tuple (gen/vector gen/small-integer)))
+
+(def test-data
+  [{:expected 20
+    :arguments [[1, 2, 3, 4]]}
+   {:expected 264
+    :arguments [[2, 2, 16, -1]]}
+   {:expected 0
+    :arguments [[1, 0, -1, -5]]}
+   {:expected 48
+    :arguments [[4, 4, 4]]}
+   {:expected 48
+    :arguments [[4, 4, 4]]}])
+
+(defn solution [arr]
+  (reduce (fn [p num] (+ p (* num num))) 0 (filter even? arr)))
