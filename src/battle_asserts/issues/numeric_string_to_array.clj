c29f44026727fb4b7226b3af4911776abc2e86c4
solar05 <artem00298@gmail.com>
2020-08-11 13:06:22 +0300

    Add solution for numeric string to array issue

---
+++ src/battle_asserts/issues/numeric_string_to_array.clj
@@ -0,0 +1,33 @@
+(ns battle-asserts.issues.numeric-string-to-array
+  (:require [clojure.string :as s]
+            [clojure.test.check.generators :as gen]
+            [faker.generate :as faker]
+            [battle-asserts.utility :as utility]))
+
+(def level :elementary)
+
+(def description "Convert a numeric string to array of numbers.
+                  It is granted that numbers in array are non-negative.")
+
+(def signature
+  {:input [{:argument-name "str" :type {:name "string"}}]
+   :output {:type {:name "array" :nested {:name "integer"}}}})
+
+(defn arguments-generator []
+  (letfn [(input-string []
+                        (s/join (repeatedly (rand-int 15) #(rand-int 10))))]
+    (gen/tuple (gen/elements (repeatedly 50 input-string)))))
+
+(def test-data
+  [{:expected [1 2 3]
+    :arguments ["123"]}
+   {:expected [3 0 9 0 2]
+    :arguments ["30902"]}
+   {:expected [1 0 1 0 0]
+    :arguments ["10100"]}])
+
+(defn solution [str]
+  (if (s/blank? str)
+    []
+    (vec (map #(Integer/parseInt %) (s/split str #"")))))
+
---
+++ test/battle_asserts/issues/numeric_string_to_array_test.clj
@@ -0,0 +1,17 @@
+(ns battle-asserts.issues.numeric-string-to-array-test
+  (:require [clojure.test :refer :all]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.clojure-test :as ct]
+            [test-helper :as h]
+            [battle-asserts.issues.numeric-string-to-array :as issue]))
+
+(ct/defspec spec-solution
+  20
+  (prop/for-all [v (issue/arguments-generator)]
+                (let [result (apply issue/solution v)]
+                  (if (nil? result)
+                    true
+                    (vector? result)))))
+
+(deftest test-solution
+  (h/generate-tests issue/test-data issue/solution))
