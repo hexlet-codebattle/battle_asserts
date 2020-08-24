7b01a07f53e109d5101e6a246d8ba09b72379ce4
solar05 <artem00298@gmail.com>
2020-08-24 23:55:37 +0300

    Add solution for secret society issue

---
+++ src/battle_asserts/issues/secret_society.clj
@@ -0,0 +1,36 @@
+(ns battle-asserts.issues.secret-society
+  (:require [clojure.string :as s]
+            [clojure.test.check.generators :as gen]
+            [faker.generate :as faker]))
+
+(def level :elementary)
+
+(def description "A group of friends have decided to start a secret society.
+                  The name will be the first letter of each of their names.
+                  Create a function that takes an array of names and returns the name of the secret society sorted in alphabetical order.")
+
+(def signature
+  {:input [{:argument-name "names" :type {:name "array" :nested {:name "string"}}}]
+   :output {:type {:name "string"}}})
+
+(defn arguments-generator
+  []
+  (gen/tuple (apply gen/tuple
+                    (repeatedly
+                     (gen/generate (gen/choose 2 10))
+                     #(gen/elements (faker/words {:lang :en}))))))
+
+;(gen/tuple (apply gen/tuple (repeatedly 6 #(gen/elements words))))
+
+(def test-data
+  [{:arguments [["Malcolm" "Adam" "Sarah"]]
+    :expected  "AMS"}
+   {:arguments [["Harry" "Ross" "Chandler" "Joey" "Rachel"]]
+    :expected  "CHJRR"}])
+
+(defn solution [names]
+  (let [sorted-names (sort names)]
+    (reduce (fn [acc name] (str
+                            acc
+                            (s/upper-case (first name))))
+            "" sorted-names)))
