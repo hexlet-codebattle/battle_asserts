65013398c62eba7deaae29da1fd389afad7b3b9d
solar05 <artem00298@gmail.com>
2020-08-25 16:22:05 +0300

    Add spec tests for anagram-finder

--- test/battle_asserts/issues/anagram_finder_test.clj
+++ test/battle_asserts/issues/anagram_finder_test.clj
@@ -1,7 +1,16 @@
 (ns battle-asserts.issues.anagram-finder-test
   (:require [clojure.test :refer :all]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.clojure-test :as ct]
             [test-helper :as h]
             [battle-asserts.issues.anagram-finder :as issue]))
+
+(ct/defspec spec-solution
+  20
+  (prop/for-all [v (issue/arguments-generator)]
+                (let [result (apply issue/solution v)]
+                  (do (vector? result)
+                      (= (count (filter vector? result)) (count result))))))
 
 (deftest test-solution
   (h/generate-tests issue/test-data issue/solution))
