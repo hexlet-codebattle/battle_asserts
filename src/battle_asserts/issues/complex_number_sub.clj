d9b752e15ef8258074e755749d957e01a3b0a1b6
solar05 <artem00298@gmail.com>
2020-08-29 01:54:33 +0300

    Refactor solution for complex sub task

--- src/battle_asserts/issues/complex_number_sub.clj
+++ src/battle_asserts/issues/complex_number_sub.clj
@@ -3,12 +3,13 @@
 
 (def level :medium)
 
-(def description "Implement a function that calculates difference of two complex numbers.")
+(def description "Implement a function that calculates difference of two complex numbers.
+                  Return result as array where first number is real part and second number is imaginary part.")
 
 (def signature
   {:input  [{:argument-name "first-num" :type {:name "array" :nested {:name "integer"}}}
             {:argument-name "second-num" :type {:name "array" :nested {:name "integer"}}}]
-   :output {:type {:name "string"}}})
+   :output {:type {:name "array" :nested {:name "integer"}}}})
 
 (defn arguments-generator
   []
@@ -16,17 +17,12 @@
              (gen/tuple gen/small-integer gen/small-integer)))
 
 (def test-data
-  [{:expected "2-1i"
-    :arguments [[5 -2], [3 -1]]}
-   {:expected "-5+2i"
-    :arguments [[-3 3], [2 1]]}
-   {:expected "1+3i"
-    :arguments [[3 4], [2 1]]}
-   {:expected "-1-3i"
-    :arguments [[-3 -4], [-2 -1]]}])
+  [{:expected [2 -1] :arguments [[5 -2] [3 -1]]}
+   {:expected [-5 2] :arguments [[-3 3] [2 1]]}
+   {:expected [1 3] :arguments [[3 4] [2 1]]}
+   {:expected [-1 -3] :arguments [[-3 -4] [-2 -1]]}])
 
 (defn solution [[real1 img1] [real2 img2]]
   (letfn [(real-part [pair] (- (first pair) (last pair)))
-          (imaginary-part [pair] (- (first pair) (last pair)))
-          (format-imaginary [num] (if (>= num 0) (str "+" num) num))]
-    (str (real-part [real1 real2]) (format-imaginary (imaginary-part [img1 img2])) "i")))
+          (imaginary-part [pair] (- (first pair) (last pair)))]
+    [(real-part [real1 real2]) (imaginary-part [img1 img2])]))
