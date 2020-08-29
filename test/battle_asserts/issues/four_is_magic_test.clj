51ac8cb925d69948e1e0adfaf087761883c4c6f6
solar05 <artem00298@gmail.com>
2020-08-30 00:03:13 +0300

    Add solution for four is magic task

---
+++ test/battle_asserts/issues/four_is_magic_test.clj
@@ -0,0 +1,14 @@
+(ns battle-asserts.issues.four-is-magic-test
+  (:require [clojure.test :refer :all]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.clojure-test :as ct]
+            [test-helper :as h]
+            [battle-asserts.issues.four-is-magic :as issue]))
+
+(ct/defspec spec-solution
+  20
+  (prop/for-all [v (issue/arguments-generator)]
+                (string? (apply issue/solution v))))
+
+(deftest test-solution
+  (h/generate-tests issue/test-data issue/solution))
