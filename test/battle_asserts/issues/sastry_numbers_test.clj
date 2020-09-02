42c4eacd052d7296f753a18854d1f556279d4b1e
Artem Solomatin <artem00298@gmail.com>
2020-09-02 14:59:13 +0300

    Add solution for sastry numbers task (#496)

---
+++ test/battle_asserts/issues/sastry_numbers_test.clj
@@ -0,0 +1,14 @@
+(ns battle-asserts.issues.sastry-numbers-test
+  (:require [clojure.test :refer :all]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.clojure-test :as ct]
+            [test-helper :as h]
+            [battle-asserts.issues.sastry-numbers :as issue]))
+
+(ct/defspec spec-solution
+  20
+  (prop/for-all [v (issue/arguments-generator)]
+                (instance? Boolean (apply issue/solution v))))
+
+(deftest test-solution
+  (h/generate-tests issue/test-data issue/solution))
