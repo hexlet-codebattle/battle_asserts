f074e4156a801ad091411435c9c3ea8b08ddc4d9
Artem Solomatin <artem00298@gmail.com>
2020-09-02 14:59:41 +0300

    Add solution for same bread task (#492)
    * Add solution for same bread task
    
    * Update same_bread.clj

---
+++ src/battle_asserts/issues/same_bread.clj
@@ -0,0 +1,31 @@
+(ns battle-asserts.issues.same-bread
+  (:require [clojure.test.check.generators :as gen]))
+
+(def level :elementary)
+
+(def description "Two arrays, which represent two sandwiches are given.
+                  Return whether both sandwiches use the same type of bread.")
+
+(def signature
+  {:input [{:argument-name "first" :type {:name "array" :nested {:name "string"}}}
+           {:argument-name "second" :type {:name "array" :nested {:name "string"}}}]
+   :output {:type {:name "boolean"}}})
+
+(defn arguments-generator []
+  (let [breads ["white bread" "brown bread" "toast"]
+        fillings ["tomato" "chicken" "ham" "cheese"]]
+    (gen/tuple (gen/tuple (gen/elements breads) (gen/elements fillings) (gen/elements breads))
+               (gen/tuple (gen/elements breads) (gen/elements fillings) (gen/elements breads)))))
+
+(def test-data
+  [{:expected true
+    :arguments [["white bread" "tomato" "white bread"] ["white bread" "chicken" "white bread"]]}
+   {:expected false
+    :arguments [["toast" "chicken" "brown bread"] ["brown bread" "chicken" "white bread"]]}
+   {:expected true
+    :arguments [["toast" "cheese" "toast"] ["toast" "ham" "toast"]]}])
+
+(defn solution [first-sandwich second-sandwich]
+  (let [first-bread-pair (vector (first first-sandwich) (last first-sandwich))
+        second-bread-pair (vector (first second-sandwich) (last second-sandwich))]
+    (= first-bread-pair second-bread-pair)))
