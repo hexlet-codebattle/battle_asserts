78c6fc519e57e9a65f8332527cc6a3e3cc42eeb0
Artem Solomatin <artem00298@gmail.com>
2020-09-02 14:57:49 +0300

    Complex number mul (#506)
    * Add complex num mul task
    
    * Update complex_number_mul.clj
    
    * Refactored solution
    
    * Increase difficulty and fix signature for complex number multiplication task
    
    * Refactor solution of complex mul task

--- src/battle_asserts/issues/complex_number_mul.clj
+++ src/battle_asserts/issues/complex_number_mul.clj
@@ -3,12 +3,13 @@
 
 (def level :medium)
 
-(def description "Implement a function that multiply two complex numbers.")
+(def description "Implement a function that multiply two complex numbers.
+                  Return result as array where first number is real part and second number is imaginary part.")
 
 (def signature
   {:input  [{:argument-name "first-num" :type {:name "array" :nested {:name "integer"}}}
             {:argument-name "second-num" :type {:name "array" :nested {:name "integer"}}}]
-   :output {:type {:name "string"}}})
+   :output {:type {:name "array" :nested {:name "integer"}}}})
 
 (defn arguments-generator
   []
@@ -16,17 +17,12 @@
              (gen/tuple gen/small-integer gen/small-integer)))
 
 (def test-data
-  [{:expected "1-7i"
-    :arguments [[1 -2], [3 -1]]}
-   {:expected "-8+1i"
-    :arguments [[-3 2], [2 1]]}
-   {:expected "4+7i"
-    :arguments [[3 2], [2 1]]}
-   {:expected "-5-1i"
-    :arguments [[2 3], [-1 1]]}])
+  [{:expected [1 -7] :arguments [[1 -2] [3 -1]]}
+   {:expected [-8 1] :arguments [[-3 2] [2 1]]}
+   {:expected [4 7] :arguments [[3 2] [2 1]]}
+   {:expected [-5 -1] :arguments [[2 3] [-1 1]]}])
 
 (defn solution [[real1 img1] [real2 img2]]
   (letfn [(real-part [first-pair second-pair] (- (* (first first-pair) (first second-pair)) (* (last first-pair) (last second-pair))))
-          (imaginary-part [first-pair second-pair] (+ (* (first first-pair) (last second-pair)) (* (last first-pair) (first second-pair))))
-          (format-imaginary [num] (if (>= num 0) (str "+" num) num))]
-    (str (real-part [real1 img1] [real2 img2]) (format-imaginary (imaginary-part [real1 img1] [real2 img2])) "i")))
+          (imaginary-part [first-pair second-pair] (+ (* (first first-pair) (last second-pair)) (* (last first-pair) (first second-pair))))]
+    [(real-part [real1 img1] [real2 img2]) (imaginary-part [real1 img1] [real2 img2])]))
