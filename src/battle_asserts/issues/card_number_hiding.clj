57496b251c10dcc7840a6303b8245cde687632ed
Artem Solomatin <artem00298@gmail.com>
2020-09-02 15:01:11 +0300

    Add solution for card number hiding issue (#476)

---
+++ src/battle_asserts/issues/card_number_hiding.clj
@@ -0,0 +1,34 @@
+(ns battle-asserts.issues.card-number-hiding
+  (:require [clojure.string :as s]
+            [clojure.test.check.generators :as gen]))
+
+(def level :elementary)
+
+(def description "Write a function that takes a credit card number
+                  and only displays the last four characters.
+                  The rest of the card number must be replaced by *.")
+
+(def signature
+  {:input  [{:argument-name "card-number" :type {:name "string"}}]
+   :output {:type {:name "string"}}})
+
+(defn arguments-generator []
+  (letfn [(card-numbers []
+            (s/join (repeatedly (gen/generate (gen/choose 8 15)) #(rand-int 10))))]
+    (gen/tuple (gen/elements (repeatedly 5 card-numbers)))))
+
+(def test-data
+  [{:expected "******3456"
+    :arguments ["1234123456"]}
+   {:expected "*************2121"
+    :arguments ["12341234561212121"]}
+   {:expected "2134"
+    :arguments ["2134"]}
+   {:expected "*6814"
+    :arguments ["76814"]}])
+
+(defn solution [card-number]
+  (let [length (count card-number)
+        last-digits-pos (- length 4)
+        last-digits (subs card-number last-digits-pos length)]
+    (str (s/join "" (repeat last-digits-pos "*")) last-digits)))
