da6cf15eab6f9d881afcc5294028e6152a141c03
solar05 <artem00298@gmail.com>
2020-09-02 21:48:28 +0300

    Fix tests

--- src/battle_asserts/issues/complex_number_div.clj
+++ src/battle_asserts/issues/complex_number_div.clj
@@ -23,13 +23,15 @@
    {:expected [-2 -1] :arguments [[30 11] [-12 1]]}])
 
 (defn solution [[real1 img1] [real2 img2]]
-  (letfn [(real-part [first-pair second-pair]
-            (int  (/ (+ (* (first first-pair) (first second-pair)) (* (last first-pair) (last second-pair)))
-                     (+ (* (first second-pair) (first second-pair)) (* (last second-pair) (last second-pair))))))
-          (imaginary-part [first-pair second-pair]
-            (int (/ (- (* (last first-pair) (first second-pair)) (* (first first-pair) (last second-pair)))
-                    (+ (* (first second-pair) (first second-pair)) (* (last second-pair) (last second-pair))))))]
-    (let
-     [real (real-part [real1 img1] [real2 img2])
-      img  (imaginary-part [real1 img1] [real2 img2])]
-      [real img])))
+  (try
+    (letfn [(real-part [first-pair second-pair]
+              (int  (/ (+ (* (first first-pair) (first second-pair)) (* (last first-pair) (last second-pair)))
+                       (+ (* (first second-pair) (first second-pair)) (* (last second-pair) (last second-pair))))))
+            (imaginary-part [first-pair second-pair]
+              (int (/ (- (* (last first-pair) (first second-pair)) (* (first first-pair) (last second-pair)))
+                      (+ (* (first second-pair) (first second-pair)) (* (last second-pair) (last second-pair))))))]
+      (let
+       [real (real-part [real1 img1] [real2 img2])
+        img  (imaginary-part [real1 img1] [real2 img2])]
+        [real img]))
+    (catch ArithmeticException e [0 0])))
