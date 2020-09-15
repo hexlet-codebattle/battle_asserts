8309673c33acb0b6b8190a19c8040b6672f488d3
solar05 <artem00298@gmail.com>
2020-09-15 17:15:48 +0300

    Add tests for signatures part 5

--- test/battle_asserts/issues/count_smaller_elements_on_right_side_test.clj
+++ test/battle_asserts/issues/count_smaller_elements_on_right_side_test.clj
@@ -13,5 +13,10 @@
                   (or (nil? last-element)
                       (zero? last-element)))))
 
+(ct/defspec spec-signature
+  20
+  (prop/for-all [v (issue/arguments-generator)]
+                (true? (h/generate-signatures issue/signature v))))
+
 (deftest test-solution
   (h/generate-tests issue/test-data issue/solution))
