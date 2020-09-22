855662961d3df6f2399f8ead5f63f37e171e1fee
solar05 <artem00298@gmail.com>
2020-09-22 19:02:00 +0300

    Add atm task

---
+++ src/battle_asserts/issues/atm_pin_code.clj
@@ -0,0 +1,30 @@
+(ns battle-asserts.issues.atm-pin-code
+  (:require [clojure.test.check.generators :as gen]
+            [faker.generate :as faker]))
+
+(def level :easy)
+
+(def description "ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+                  Create a function that takes a string and check if the PIN is valid.")
+
+(def signature
+  {:input [{:argument-name "pin" :type {:name "string"}}]
+   :output {:type {:name "boolean"}}})
+
+(defn arguments-generator []
+  (letfn [(four-digit-code [] (str (gen/generate (gen/choose 1000 9999))))
+          (six-digit-code [] (str (gen/generate (gen/choose 100000 999999))))
+          (invalid-code [] (gen/generate (gen/elements (faker/words {:lang :en :n 10}))))]
+    (gen/tuple (gen/elements [(four-digit-code) (six-digit-code) (invalid-code)]))))
+
+(def test-data
+  [{:expected true :arguments ["1234"]}
+   {:expected true :arguments ["123456"]}
+   {:expected false :arguments ["12345"]}
+   {:expected false :arguments ["some pin code"]}
+   {:expected false :arguments ["m8te"]}])
+
+(defn solution [code]
+  (letfn [(only-digit? [pin] (not (empty? (re-matches #"^[0-9]+$" pin))))]
+    (let [len (count code)]
+      (and (only-digit? code) (or (= len 4) (= len 6))))))
