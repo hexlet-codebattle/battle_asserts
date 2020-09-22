00cecb2ee7660ba326e4da0774f1c9bff0582195
solar05 <artem00298@gmail.com>
2020-09-22 23:21:24 +0300

    Add number in range task

---
+++ src/battle_asserts/issues/number_range.clj
@@ -0,0 +1,24 @@
+(ns battle-asserts.issues.number-range
+  (:require [clojure.test.check.generators :as gen]))
+
+(def level :elementary)
+
+(def description "Create a function that validates whether a number `n` is exclusively within the bounds of `lower` and `upper`.")
+
+(def signature
+  {:input [{:argument-name "num" :type {:name "integer"}}
+           {:argument-name "lower" :type {:name "integer"}}
+           {:argument-name "upper" :type {:name "integer"}}]
+   :output {:type {:name "boolean"}}})
+
+(defn arguments-generator []
+  (gen/tuple (gen/choose 0 30) (gen/choose 0 30) (gen/choose 0 30)))
+
+(def test-data
+  [{:expected true :arguments [3 1 9]}
+   {:expected false :arguments [6 1 6]}
+   {:expected true :arguments [11 1 20]}
+   {:expected false :arguments [4 11 15]}])
+
+(defn solution [num left-border right-border]
+  (contains? (set (range (inc left-border) right-border)) num))
