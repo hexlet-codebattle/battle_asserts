d35590683158408a7a8398fddcd44fc85644aa49
solar05 <artem00298@gmail.com>
2020-09-23 01:53:47 +0300

    Add orthogonal vectors task

---
+++ src/battle_asserts/issues/orthogonal_vectors.clj
@@ -0,0 +1,26 @@
+(ns battle-asserts.issues.orthogonal-vectors
+  (:require [clojure.test.check.generators :as gen]))
+
+(def level :medium)
+
+(def description "Create a function that takes two vectors and checks if them are orthogonal or not.
+                  Two vectors are orthogonal if their dot product is equal to zero.")
+
+(def signature
+  {:input  [{:argument-name "vec1" :type {:name "array" :nested {:name "integer"}}}
+            {:argument-name "vec2" :type {:name "array" :nested {:name "integer"}}}]
+   :output {:type {:name "boolean"}}})
+
+(defn arguments-generator []
+  (gen/tuple (gen/vector (gen/choose -3 3) 2)
+             (gen/vector (gen/choose -3 3) 2)))
+
+(def test-data
+  [{:expected true :arguments [[1 2] [2 -1]]}
+   {:expected true :arguments [[3 -3] [3 3]]}
+   {:expected false :arguments [[3 -1] [3 0]]}])
+
+(defn solution [first-vec second-vec]
+  (let [zipped (map vector first-vec second-vec)
+        dot-product (reduce (fn [acc [first-elem second-elem]] (+ acc (* first-elem second-elem))) 0 zipped)]
+    (zero? dot-product)))
