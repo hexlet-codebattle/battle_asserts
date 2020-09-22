9e5c918a5c516db0a4cbc4dee6f14430d72ac9af
solar05 <artem00298@gmail.com>
2020-09-22 20:22:18 +0300

    Add year to century task

---
+++ src/battle_asserts/issues/year_to_century.clj
@@ -0,0 +1,23 @@
+(ns battle-asserts.issues.year-to-century
+  (:require [clojure.test.check.generators :as gen]))
+
+(def level :elementary)
+
+(def description "Create a function that takes a year and returns it's corresponding century.")
+
+(def signature
+  {:input [{:argument-name "year" :type {:name "integer"}}]
+   :output {:type {:name "integer"}}})
+
+(defn arguments-generator
+  []
+  (gen/tuple (gen/choose 1900 3000)))
+
+(def test-data
+  [{:expected 21 :arguments [2005]}
+   {:expected 20 :arguments [1950]}
+   {:expected 19 :arguments [1900]}
+   {:expected 21 :arguments [2055]}])
+
+(defn solution [year]
+  (int (Math/ceil (/ year 100))))
