c14dbeb6c49fb5ff21b830b6586ffd0841cbd680
solar05 <artem00298@gmail.com>
2020-09-29 01:33:19 +0300

    Add repdigit number task

---
+++ src/battle_asserts/issues/repdigit_number.clj
@@ -0,0 +1,28 @@
+(ns battle-asserts.issues.repdigit-number
+  (:require [clojure.test.check.generators :as gen]
+            [clojure.string :as s]))
+
+(def level :elementary)
+
+(def description "A repdigit is a positive number composed out of the same digit.
+                  Create a function that checks an integer it's a repdigit or not.")
+
+(def signature
+  {:input [{:argument-name "number" :type {:name "integer"}}]
+   :output {:type {:name "boolean"}}})
+
+(defn arguments-generator []
+  (let [repdigits (gen/elements [11 111 1111 22 222 2222 33 333 3333 3333 44 444 444 4444])]
+    (gen/tuple (gen/one-of [(gen/choose 10 1000) repdigits]))))
+
+(def test-data
+  [{:expected true :arguments [66]}
+   {:expected true :arguments [111]}
+   {:expected false :arguments [112]}
+   {:expected false :arguments [10]}])
+
+(defn solution [number]
+  (let [stringified (str number)
+        start-number (first stringified)
+        filtered-num (s/join "" (filter (fn [ch] (= ch start-number)) stringified))]
+    (= stringified filtered-num)))
