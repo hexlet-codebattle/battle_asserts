2c6f635955f3aade325a002bf871becd97ea710b
solar05 <artem00298@gmail.com>
2020-10-01 21:40:01 +0300

    Add sort by length task

---
+++ test/battle_asserts/issues/sort_by_length_test.clj
@@ -0,0 +1,21 @@
+(ns battle-asserts.issues.sort-by-length-test
+  (:require [clojure.test :refer :all]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.clojure-test :as ct]
+            [test-helper :as h]
+            [battle-asserts.issues.sort-by-length :as issue]))
+
+(ct/defspec spec-solution
+  20
+  (prop/for-all [v (issue/arguments-generator)]
+                (print v)
+                (vector? (apply issue/solution v))))
+
+(ct/defspec spec-signature
+  20
+  (prop/for-all [v (issue/arguments-generator)]
+                (true? (h/generate-signatures issue/signature v))))
+
+(deftest test-solution
+  (h/generate-tests issue/test-data issue/solution))
+
