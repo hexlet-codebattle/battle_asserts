2ed77a1a47e979bee2d105566c5d7bf58d1bd45c
solar05 <artem00298@gmail.com>
2020-10-01 22:25:26 +0300

    Add how heavy task

---
+++ src/battle_asserts/issues/how_heavy.clj
@@ -0,0 +1,22 @@
+(ns battle-asserts.issues.how-heavy
+  (:require [clojure.test.check.generators :as gen]))
+
+(def level :easy)
+
+(def description "Given `radius` and `height` (in cm), calculate the mass of a cylinder when it's filled with water and the cylinder itself doesn't weigh anything. Use ceil rounding for answer.")
+
+(def signature
+  {:input [{:argument-name "radius" :type {:name "integer"}}
+           {:argument-name "height" :type {:name "integer"}}]
+   :output {:type {:name "integer"}}})
+
+(defn arguments-generator []
+  (gen/tuple (gen/choose 1 20) (gen/choose 1 100)))
+
+(def test-data
+  [{:expected 1 :arguments [4 10]}
+   {:expected 8 :arguments [15 10]}
+   {:expected 170 :arguments [30 60]}])
+
+(defn solution [radius height]
+  (int (Math/ceil (* Math/PI (Math/pow radius 2) height 0.001))))
