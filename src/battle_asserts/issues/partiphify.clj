305225c102bc4c9b5bfa1a388c6fce7e6564780d
vtm9 <vtmilyakov@yandex.ru>
2020-10-03 17:15:37 +0300

    Add partiphify

---
+++ src/battle_asserts/issues/partiphify.clj
@@ -0,0 +1,16 @@
+(ns battle-asserts.issues.partiphify
+  (:require [clojure.test.check.generators :as gen]))
+
+(def level :elementary)
+
+(def description "Create a function that will divide the list by a specified number of parts")
+
+(def signature
+  {:input  [{:argument-name "integers" :type {:name "array" :nested {:name "integer"}}}
+            {:argument-name "parts" :type {:name "integer"}}]
+   :output {:type :type {:name "array" :nested {:name "integer"}}}})
+
+(def test-data
+  [{:expected [[1], []] :arguments [[1], 2]}
+   {:expected [[1] [2] [3]] :arguments [[1 2 3], 3]}
+   {:expected [[1 3 5] [2 4]] :arguments [[1 2 3 4 5], 2]}
