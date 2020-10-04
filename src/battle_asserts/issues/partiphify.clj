ff9dd9557250fd23792855c88f988a040072b37b
solar05 <artem00298@gmail.com>
2020-10-04 15:54:55 +0300

    Add solution for partiphify task

--- src/battle_asserts/issues/partiphify.clj
+++ src/battle_asserts/issues/partiphify.clj
@@ -3,14 +3,24 @@
 
 (def level :elementary)
 
-(def description "Create a function that will divide the list by a specified number of parts")
+(def description "Create a function that will divide the list by a specified number of parts.")
 
 (def signature
-  {:input  [{:argument-name "integers" :type {:name "array" :nested {:name "integer"}}}
+  {:input  [{:argument-name "numbers" :type {:name "array" :nested {:name "integer"}}}
             {:argument-name "parts" :type {:name "integer"}}]
    :output {:type {:name "array" :nested {:name "integer"}}}})
 
+(defn arguments-generator []
+  (gen/tuple (gen/vector gen/small-integer 2 10) (gen/choose 2 4)))
+
 (def test-data
-  [{:expected [[1], []] :arguments [[1], 2]}
-   {:expected [[1] [2] [3]] :arguments [[1 2 3], 3]}
-   {:expected [[1 3 5] [2 4]] :arguments [[1 2 3 4 5], 2]}])
+  [{:expected [[1] []] :arguments [[1] 2]}
+   {:expected [[1] [2] [3]] :arguments [[1 2 3] 3]}
+   {:expected [[1 2 3] [4 5]] :arguments [[1 2 3 4 5] 2]}])
+
+(defn solution [numbers parts]
+  (let [part (int (Math/ceil (/ (count numbers) parts)))
+        divided-vec (vec (map vec (partition-all part numbers)))
+        final-vec (if (not= (count divided-vec) parts) (conj divided-vec []) divided-vec)]
+    final-vec))
+
