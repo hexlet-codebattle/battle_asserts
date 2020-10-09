57946463b06b653ffcfcf71215d56822a9dca10d
Alexandr Maniunin <aamanunin@gmail.com>
2020-10-09 10:43:56 +0700

    fix desc_order description

--- src/battle_asserts/issues/desc_order.clj
+++ src/battle_asserts/issues/desc_order.clj
@@ -3,7 +3,7 @@
 
 (def level :elementary)
 
-(def description "Your tast is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order.
+(def description "Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order.
   Essentially, rearrange the digits to create the highest possible number.")
 
 (def signature
