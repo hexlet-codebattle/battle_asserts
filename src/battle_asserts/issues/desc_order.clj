ac8e945742dbbf7ef84848285d32e67c45ab999f
solar05 <artem00298@gmail.com>
2020-10-11 02:50:28 +0300

    Add solution for desc order and fix one test

--- src/battle_asserts/issues/desc_order.clj
+++ src/battle_asserts/issues/desc_order.clj
@@ -1,5 +1,6 @@
 (ns battle-asserts.issues.desc-order
-  (:require [clojure.test.check.generators :as gen]))
+  (:require [clojure.test.check.generators :as gen]
+            [clojure.string :as s]))
 
 (def level :elementary)
 
@@ -11,9 +12,13 @@
    :output {:type {:name "integer"}}})
 
 (def test-data
-  [{:expected 54421
-    :arguments [42145]}
-   {:expected 654321
-    :arguments [145263]}
-   {:expected 123456789
-    :arguments [987654321]}])
+  [{:expected 0 :arguments [0]}
+   {:expected 54421 :arguments [42145]}
+   {:expected 654321 :arguments [145263]}
+   {:expected 987654321 :arguments [987654321]}])
+
+(defn arguments-generator []
+  (gen/tuple (gen/choose 0 1000000000)))
+
+(defn solution [num]
+  (Integer/parseInt (s/join "" (reverse (sort (str num))))))
