14c2012395231dae1c9edbf97eed9e1463f7e51a
solar05 <artem00298@gmail.com>
2020-10-11 03:14:39 +0300

    Add how many digits task

---
+++ src/battle_asserts/issues/how_many_digits.clj
@@ -0,0 +1,22 @@
+(ns battle-asserts.issues.how-many-digits
+  (:require [clojure.test.check.generators :as gen]))
+
+(def level :elementary)
+
+(def description "Imagine you took all the numbers between 0 and n and concatenated them together into a long string. How many digits are there between 0 and n? Write a function that can calculate this.")
+
+(def signature
+  {:input  [{:argument-name "num" :type {:name "integer"}}]
+   :output {:type {:name "integer"}}})
+
+(def test-data
+  [{:expected 0 :arguments [1]}
+   {:expected 9 :arguments [10]}
+   {:expected 189 :arguments [100]}
+   {:expected 6973 :arguments [2021]}])
+
+(defn arguments-generator []
+  (gen/tuple (gen/choose 0 100000)))
+
+(defn solution [num]
+  (count (apply str (range 1 num))))
