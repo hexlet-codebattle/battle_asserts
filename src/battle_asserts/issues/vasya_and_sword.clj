c28a0b26e87401738a32eab94b1e09f39779c8a5
Artem Solomatin <artem00298@gmail.com>
2020-11-17 04:23:02 +0300

    Fix idents for description

--- src/battle_asserts/issues/vasya_and_sword.clj
+++ src/battle_asserts/issues/vasya_and_sword.clj
@@ -6,9 +6,8 @@
 (def description "Vasya has earned a lot of virtual coins and wants to buy for them a unique sword in the in-game store of his favorite MMORPG. The store is arranged in such a way that the prices of items in it change every day. Today a sword can cost 100 coins, tomorrow - 70, the day after tomorrow - 150.
                   Vasya sword is only needed for a while to make a quest with friends, in the future he is going to sell it with the same store. However, there is another trick in the store: once a player buys an item for a certain price, he cannot sell it in the future for a higher price or equal to the one he made the purchase for. For example, if Vasya buys a sword for 100 coins, he can't sell it for 150 coins.
                   Vasya has got his hands on the sword's price schedule for the next few days, so help him make a deal where he will suffer the minimum loss from resale.
-
                   The function accepts an array of integer prices for the nearest sequence of `n` days (n < 1000).
-Returns the minimum loss from resale.")
+                  Returns the minimum loss from resale.")
 
 (def signature
   {:input  [{:argument-name "prices" :type {:name "array" :nested {:name "integer"}}}]
