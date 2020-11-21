0c0d8056e55f7d1c505e9ea1dbdf10ba332dbee7
solar05 <artem00298@gmail.com>
2020-11-21 11:47:47 +0300

    Odd all way fix

--- src/battle_asserts/issues/odd_all_way.clj
+++ src/battle_asserts/issues/odd_all_way.clj
@@ -6,7 +6,7 @@
 (def disabled true)
 
 (def description "Given an array of numbers, return an array which contains all the odd numbers in the orginal array,
-                  which also have odd indexes.")
+                  which also have odd indexes. If there is no such numbers, return `[0]`.")
 
 (def signature
   {:input [{:argument-name "numbers" :type {:name "array" :nested {:name "integer"}}}]
@@ -16,16 +16,20 @@
   (gen/tuple (gen/vector (gen/choose -3 3) 9 15)))
 
 (def test-data
-  [{:expected [3 5] :arguments [[1 3 3 5 5]]}
+  [{:expected [0] :arguments [[1 2 3]]}
+   {:expected [3 5] :arguments [[1 3 3 5 5]]}
    {:expected [3 11 15] :arguments [[1 3 1 6 9 11 13 15]]}
    {:expected [1] :arguments [[0 1 2 2 4]]}])
 
 (defn solution [numbers]
-  (reduce (fn [acc elem]
-            (let [index (first elem)
-                  number (second elem)]
-              (if (and (odd? index) (odd? number))
-                (conj acc number)
-                acc)))
-          []
-          (map-indexed vector numbers)))
+  (let [result (reduce (fn [acc elem]
+                         (let [index (first elem)
+                               number (second elem)]
+                           (if (and (odd? index) (odd? number))
+                             (conj acc number)
+                             acc)))
+                       []
+                       (map-indexed vector numbers))]
+    (if (empty? result)
+      [0]
+      result)))
