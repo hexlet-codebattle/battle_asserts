f5de37b6f4587919e50b32b4dc92ae23c70a0083
solar05 <artem00298@gmail.com>
2020-11-22 01:35:29 +0300

    Add longest strings task

---
+++ src/battle_asserts/issues/longest_strings.clj
@@ -0,0 +1,27 @@
+(ns battle-asserts.issues.first-repeating-element
+  (:require [clojure.test.check.generators :as gen]
+            [faker.generate :as faker]))
+
+(def level :elementary)
+
+(def description "Given an array of strings, write the function, that return another array, containing all of its longest strings.")
+
+(def signature
+  {:input  [{:argument-name "words" :type {:name "array" :nested {:name "string"}}}]
+   :output {:type {:name "array" :nested {:name "string"}}}})
+
+(defn arguments-generator []
+  (gen/tuple (gen/vector (gen/elements (faker/words {:n 20})) 5 15)))
+
+(def test-data
+  [{:expected ["programms"]
+    :arguments [["in" "Soviet" "Russia" "frontend" "programms" "you"]]}
+   {:expected ["clojure" "better"]
+    :arguments [["using" "clojure" "makes" "your" "life" "greater"]]}
+   {:expected ["a" "b" "c" "d"]
+    :arguments [["a" "b" "c" "d"]]}])
+
+(defn solution [words]
+  (let [sorted-words (sort-by count words)
+        longest-word (count (last sorted-words))]
+    (filter #(= (count %) longest-word) words)))
