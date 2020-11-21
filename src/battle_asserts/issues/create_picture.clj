41caa0b19e85cc7d99a7a655806510d29e0db201
solar05 <artem00298@gmail.com>
2020-11-22 02:50:21 +0300

    Add create picture task

---
+++ src/battle_asserts/issues/create_picture.clj
@@ -0,0 +1,33 @@
+(ns battle-asserts.issues.create-picture
+  (:require [clojure.test.check.generators :as gen]
+            [faker.generate :as faker]
+            [clojure.string :as s]))
+
+(def level :easy)
+
+(def description "Given an array of words, write a function, that return picture (array) with borders made from asterisks `*` using longest words.")
+
+(def signature
+  {:input  [{:argument-name "words" :type {:name "array" :nested {:name "string"}}}]
+   :output {:type {:name "array" :nested {:name "string"}}}})
+
+(defn arguments-generator []
+  (gen/tuple (gen/vector (gen/elements (faker/words {:n 20})) 2 15)))
+
+(def test-data
+  [{:expected ["*****"
+               "*def*"
+               "*****"]
+    :arguments [["a" "bc" "def"]]}
+   {:expected ["******"
+               "*some*"
+               "*word*"
+               "******"]
+    :arguments [["some" "word" "foo" "bar"]]}])
+
+(defn solution [words]
+  (let [longest-word-size (count (last (sort-by count words)))
+        picture-words (filter #(= (count %) longest-word-size) words)
+        picturized (map #(str "*" % "*") picture-words)
+        border (s/join (repeat (+ 2 longest-word-size) "*"))]
+    (conj (apply conj [border] picturized) border)))
