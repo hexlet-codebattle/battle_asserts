8eca81dfeab90d85b3311ff06ead5c70689db4ce
solar05 <artem00298@gmail.com>
2020-11-21 11:41:23 +0300

    Fix even all way task

--- src/battle_asserts/issues/even_all_way.clj
+++ src/battle_asserts/issues/even_all_way.clj
@@ -3,10 +3,8 @@
 
 (def level :easy)
 
-(def disabled true)
-
 (def description "Given an array of numbers, return an array which contains all the even numbers in the orginal array,
-                  which also have even indexes.")
+                  which also have even indexes. If there is no such numbers, return `[0]`.")
 
 (def signature
   {:input [{:argument-name "numbers" :type {:name "array" :nested {:name "integer"}}}]
@@ -16,16 +14,20 @@
   (gen/tuple (gen/vector (gen/choose -4 4) 8 16)))
 
 (def test-data
-  [{:expected [2] :arguments [[1 2 2 4 5]]}
+  [{:expected [0] :arguments [[1 2 3]]}
+   {:expected [2] :arguments [[1 2 2 4 5]]}
    {:expected [2 4] :arguments [[1 3 2 6 4 8]]}
    {:expected [0 2 4] :arguments [[0 1 2 3 4]]}])
 
 (defn solution [numbers]
-  (reduce (fn [acc elem]
-            (let [index (first elem)
-                  number (second elem)]
-              (if (and (even? index) (even? number))
-                (conj acc number)
-                acc)))
-          []
-          (map-indexed vector numbers)))
+  (let [result (reduce (fn [acc elem]
+                         (let [index (first elem)
+                               number (second elem)]
+                           (if (and (even? index) (even? number))
+                             (conj acc number)
+                             acc)))
+                       []
+                       (map-indexed vector numbers))]
+    (if (empty? result)
+      [0]
+      result)))
