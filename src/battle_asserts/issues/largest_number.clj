b1584c432bcc7485452d624d755470fdb12ebac3
Artem Solomatin <artem00298@gmail.com>
2020-11-22 23:35:07 +0200

    Add largest number task

---
+++ src/battle_asserts/issues/largest_number.clj
@@ -0,0 +1,21 @@
+(ns battle-asserts.issues.largest-number
+  (:require [clojure.test.check.generators :as gen]))
+
+(def level :elementary)
+
+(def description "Given an integer `n`, return the largest number that contains exactly `n` digits.")
+
+(def signature
+  {:input [{:argument-name "n" :type {:name "integer"}}]
+   :output {:type {:name "integer"}}})
+
+(defn arguments-generator []
+  (gen/tuple (gen/choose 1 20)))
+
+(def test-data
+  [{:expected 9 :arguments [1]}
+   {:expected 99 :arguments [2]}
+   {:expected 9999999 :arguments [7]}])
+
+(defn solution [n]
+  (dec (Math/pow 10 n)))
