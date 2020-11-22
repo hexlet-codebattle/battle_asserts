e0f0ca1738f4b144ded3f04555315fb995bfb480
solar05 <artem00298@gmail.com>
2020-11-22 19:43:46 +0300

    Add words lenghts array task

---
+++ src/battle_asserts/issues/words_lengths_array.clj
@@ -0,0 +1,22 @@
+(ns battle-asserts.issues.words-lengths-array
+  (:require [clojure.test.check.generators :as gen]
+            [faker.generate :as faker]))
+
+(def level :elementary)
+
+(def description "Create a function that takes an array of words and transforms it into an array of each word's length.")
+
+(def signature
+  {:input [{:argument-name "words" :type {:name "array" :nested {:name "string"}}}]
+   :output {:type {:name "array" :nested {:name "integer"}}}})
+
+(defn arguments-generator []
+  (gen/tuple (gen/vector (gen/elements (faker/words {:n 20})) 5 20)))
+
+(def test-data
+  [{:expected [5 5] :arguments [["hello" "world"]]}
+   {:expected [4 4 4 7] :arguments [["some" "test" "data" "strings"]]}
+   {:expected [7] :arguments [["clojure"]]}])
+
+(defn solution [words]
+  (mapv #(count %) words))
