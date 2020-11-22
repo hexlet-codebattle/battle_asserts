6571b005ceb9dfc981e772ef6768187884d17fd5
Artem Solomatin <artem00298@gmail.com>
2020-11-22 15:44:48 +0300

    Array two digit sum task (#625)
    * Add array two digit sum
    
    * Fix linter errors

---
+++ src/battle_asserts/issues/array_two_digit_sum.clj
@@ -0,0 +1,28 @@
+(ns battle-asserts.issues.array-two-digit-sum
+  (:require [clojure.test.check.generators :as gen]))
+
+(def level :elementary)
+
+(def description "Calculate digits sum of two-digit integers in array.
+                  After that, calculate whole sum of produced array.")
+
+(def signature
+  {:input [{:argument-name "numbers" :type {:name "array" :nested {:name "integer"}}}]
+   :output {:type {:name "integer"}}})
+
+(defn arguments-generator []
+  (gen/tuple (gen/vector (gen/choose 10 99) 3 10)))
+
+(def test-data
+  [{:expected 6 :arguments [[10 11 12]]}
+   {:expected 20 :arguments [[11 22 33 44]]}
+   {:expected 108 :arguments [[99 99 99 99 99 99]]}])
+
+(defn two-digit-sum [n]
+  (let [first-num (/ n 10)
+        second-num (rem n 10)]
+    (int (+ first-num second-num))))
+
+(defn solution [numbers]
+  (let [two-number-arr (map two-digit-sum numbers)]
+    (apply + two-number-arr)))
