29fc755c9473a1b80f5e77544e2d4f57e7f63314
solar05 <artem00298@gmail.com>
2020-11-22 19:29:53 +0300

    Add colour invert task

---
+++ src/battle_asserts/issues/colour_invert.clj
@@ -0,0 +1,21 @@
+(ns battle-asserts.issues.colour-invert
+  (:require [clojure.test.check.generators :as gen]))
+
+(def level :elementary)
+
+(def description "Create a function that inverts the rgb values of a given array.")
+
+(def signature
+  {:input [{:argument-name "colours" :type {:name "array" :nested {:name "integer"}}}]
+   :output {:type {:name "array" :nested {:name "integer"}}}})
+
+(defn arguments-generator []
+  (gen/tuple (gen/vector (gen/choose 0 255) 3)))
+
+(def test-data
+  [{:expected [0 0 0] :arguments [[255 255 255]]}
+   {:expected [255 255 255] :arguments [[0 0 0]]}
+   {:expected [90 85 34] :arguments [[165 170 221]]}])
+
+(defn solution [colours]
+  (mapv #(- 255 %) colours))
