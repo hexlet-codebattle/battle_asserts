1cfde26a780f6bd23e5e810e8f0e140d21f9b2b0
solar05 <artem00298@gmail.com>
2020-11-23 14:31:36 +0300

    Change result rounding

--- src/battle_asserts/issues/apply_discount.clj
+++ src/battle_asserts/issues/apply_discount.clj
@@ -3,7 +3,7 @@
 
 (def level :elementary)
 
-(def description "Create a function that applies a `discount` to every `price` of the product in the array. Use floor rounding.")
+(def description "Create a function that applies a `discount` to every `price` of the product in the array. Round result to nearest.")
 
 (def signature
   {:input [{:argument-name "prices" :type {:name "array" :nested {:name "integer"}}}
@@ -14,12 +14,12 @@
   (gen/tuple (gen/vector (gen/choose 10 9999) 5 20) (gen/choose 1 90)))
 
 (def test-data
-  [{:expected [1 2 3 5] :arguments [[2 4 6 11] 50]}
-   {:expected [8 17 35 89] :arguments [[10 20 40 100] 10]}
+  [{:expected [1 2 3 5] :arguments [[2 4 6 10] 50]}
+   {:expected [9 18 36 90] :arguments [[10 20 40 100] 10]}
    {:expected [1 2 4 10] :arguments [[10 20 40 100] 90]}])
 
 (defn apply-discount [price discount]
-  (int (Math/floor (- price (* price (float (/ discount 100)))))))
+  (int (Math/round (- price (* price (float (/ discount 100)))))))
 
 (defn solution [prices discount]
   (mapv #(apply-discount % discount) prices))
