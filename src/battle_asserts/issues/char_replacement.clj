376279f97b3a3e615548c979ae6004963e5acba9
solar05 <artem00298@gmail.com>
2020-11-29 23:18:10 +0300

    Add char replacement task

---
+++ src/battle_asserts/issues/char_replacement.clj
@@ -0,0 +1,32 @@
+(ns battle-asserts.issues.char-replacement
+  (:require [clojure.string :as s]
+            [clojure.test.check.generators :as gen]
+            [faker.generate :as faker]))
+
+(def level :elementary)
+
+(def description "Write a function thar replaces every `character` for `replacement` from every string in array.")
+
+(def signature
+  {:input [{:argument-name "words" :type {:name "array" :nested {:name "string"}}}
+           {:argument-name "character" :type {:name "string"}}
+           {:argument-name "replacement" :type {:name "string"}}]
+   :output {:type {:name "array" :nested {:name "string"}}}})
+
+(defn arguments-generator []
+  (letfn [(gen-char [] (str (gen/generate gen/char-alpha)))]
+    (gen/tuple
+     (gen/vector (gen/elements (faker/words {:n 20})) 3 10)
+     (gen/elements (repeatedly 20 gen-char))
+     (gen/elements (repeatedly 20 gen-char)))))
+
+(def test-data
+  [{:arguments [["test" "string" "example"] "t" "d"]
+    :expected  ["desd" "sdring" "example"]}
+   {:arguments [["learn" "clojure" "be" "happy"] "l" "L"]
+    :expected  ["Learn" "cLojure" "be" "happy"]}
+   {:arguments [["foo" "bar" "frontend"] "z" "Y"]
+    :expected  ["foo" "bar" "frontend"]}])
+
+(defn solution [words character replacement]
+  (mapv #(s/replace % character replacement) words))
