8bb21bf5bf78367320ad420faefd5bac3c9ca1e1
solar05 <artem00298@gmail.com>
2020-12-29 18:27:55 +0300

    Restate count words task

--- src/battle_asserts/issues/counting_words.clj
+++ src/battle_asserts/issues/counting_words.clj
@@ -5,13 +5,11 @@
 
 (def level :easy)
 
-(def disabled true)
-
-(def description "Return a hash map that shows how many times each word occurs in the given string.")
+(def description "Return a vector with subvectors that shows how many times each word occurs in the given string.")
 
 (def signature
   {:input [{:argument-name "sentence" :type {:name "string"}}]
-   :output {:type {:name "hash" :nested {:name "integer"}}}})
+   :output {:type {:name "array" :nested {:name "array" :nested {:name "string"}}}}})
 
 (defn arguments-generator []
   (let [strs (gen/vector (gen/elements (faker/words {:lang :en :n 15})) 3 10)
@@ -19,12 +17,13 @@
     (gen/tuple sentence)))
 
 (def test-data
-  [{:expected {"make" 1, "codebattle" 1, "great" 1, "again" 1}
+  [{:expected [["make" 1] ["codebattle" 1] ["great" 1] ["again" 1]]
     :arguments ["make codebattle great again"]}
-   {:expected {"codebattle" 2, "is" 2, "cool" 1, "fun" 1, "let" 2, "us" 2, "code" 1, "battle" 1}
+   {:expected [["codebattle" 2] ["is" 2] ["cool" 1] ["fun" 1] ["let" 2] ["us" 2] ["code" 1] ["battle" 1]]
     :arguments ["codebattle is cool, codebattle is fun, let us code, let us battle"]}])
 
 (defn solution [sentence]
-  (->> sentence
-       (re-seq #"\w+")
-       frequencies))
+  (let [freq (->> sentence
+                  (re-seq #"\w+")
+                  frequencies)]
+    (mapv (fn [elem] [(first elem) (last elem)]) freq)))
