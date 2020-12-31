db067e8193f58f703abf327add9ebfd5ed5a1491
solar05 <artem00298@gmail.com>
2020-12-31 18:14:55 +0300

    Refactor stairway to heaven

--- src/battle_asserts/issues/true_stairway_to_heaven.clj
+++ src/battle_asserts/issues/true_stairway_to_heaven.clj
@@ -1,11 +1,11 @@
-(ns battle-asserts.issues.stairway-to-heaven
+(ns battle-asserts.issues.true-stairway-to-heaven
   (:require [clojure.test.check.generators :as gen]))
 
 (def level :hard)
 
 (def description "`N` dicks randomly spread out of `M` stairs, there can be as many dicks as you want on one step. We gotta go down these stairs.
                   Every time you step on a stair-step with dicks, the infame number increases by the number of dicks.
-                  You can go down one or two steps at a time. Write a function to descend the stairs minimizing the infame number (function must find local minimun at each step!).
+                  You can go down one or two steps at a time. Write a function to descend the stairs minimizing the infame number.
                   The function receives an array with the number of dicks on each step and returns the minimized infame number.
                   Powered by Eugene Zaytsev.")
 
@@ -14,25 +14,25 @@
    :output {:type {:name "integer"}}})
 
 (defn arguments-generator []
-  (gen/tuple (gen/vector (gen/choose 0 12) 3 12)))
+  (gen/tuple (gen/vector (gen/choose 0 12) 8 40)))
 
 (def test-data
   [{:expected 0 :arguments [[0 1 0 1]]}
    {:expected 0 :arguments [[1 0 1 0 1]]}
-   {:expected 9 :arguments [[1 0 3 5 10 0 11 1]]}
-   {:expected 20 :arguments [[0 11 6 8 1 4 10 9]]}
-   {:expected 34 :arguments [[9 11 1 5 6 6 5 4 9 12]]}])
+   {:expected 6 :arguments [[1 0 3 5 10 0 11 1]]}
+   {:expected 17 :arguments [[0 11 6 8 1 4 10 9]]}
+   {:expected 30 :arguments [[9 11 1 5 6 6 5 4 9 12]]}])
 
-(defn iter-step
-  ([steps] (iter-step steps 0))
-  ([steps acc]
-   (let [len (count steps)]
-     (if (<= len 1) acc
-         (let [first-pol (first steps)
-               second-pol (second steps)]
-           (if (< first-pol second-pol)
-             (iter-step (drop 1 steps) (+ acc first-pol))
-             (iter-step (drop 2 steps) (+ acc second-pol))))))))
+(defn iter-step [steps]
+  (let [len (count steps)]
+    (cond
+      (= len 1) (steps 0)
+      (= len 2) (min (steps 1) (steps 0))
+      :else
+      (letfn [(iter [i acc1 acc2] (if (= i len)
+                                    (min acc1 acc2)
+                                    (iter (inc i) (+ (steps i) (min acc1 acc2)) acc1)))]
+        (iter 2 (steps 1) (steps 0))))))
 
 (defn solution [steps-map]
   (iter-step steps-map))
