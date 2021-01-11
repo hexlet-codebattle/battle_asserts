fba8c7c1b88968028302d92ada4c0e016a74b53c
solar05 <artem00298@gmail.com>
2021-01-12 02:20:43 +0300

    Fix minus zero problem

--- src/battle_asserts/issues/additive_inverse.clj
+++ src/battle_asserts/issues/additive_inverse.clj
@@ -1,7 +1,7 @@
 (ns battle-asserts.issues.additive-inverse
   (:require [clojure.test.check.generators :as gen]))
 
-(def level :easy)
+(def level :elementary)
 
 (def description "Implement a function that returns an array of additive inverses.
                   A number added with its `additive inverse` equals zero.")
@@ -10,13 +10,16 @@
   {:input [{:argument-name "numbers" :type {:name "array" :nested {:name "integer"}}}]
    :output {:type {:name "array" :nested {:name "integer"}}}})
 
-(defn arguments-generator
-  []
-  (gen/tuple (gen/vector (gen/choose -130 130) 2 6)))
+(defn arguments-generator []
+  (letfn [(neg-int [] (gen/generate (gen/choose -1000 -1)))
+          (pos-int [] (gen/generate (gen/choose 1 1000)))
+          (numbers [] (gen/elements (concat
+                                     (repeatedly 20 neg-int) (repeatedly 20 pos-int))))]
+    (gen/tuple (gen/vector (numbers) 3 10))))
 
 (def test-data
   [{:expected [-1 2 -3 4 5] :arguments [[1 -2 3 -4 -5]]}
-   {:expected [-1 -1 -1 0] :arguments [[1 1 1 0]]}
+   {:expected [-1 -1 -1 -10] :arguments [[1 1 1 10]]}
    {:expected [5 -25 -125] :arguments [[-5 25 125]]}])
 
 (defn solution [numbers]
