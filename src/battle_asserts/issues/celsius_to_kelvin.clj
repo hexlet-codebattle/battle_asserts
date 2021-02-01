d7e4301427e9f862e12b53cbde59ebb92275e438
solar05 <artem00298@gmail.com>
2021-02-01 22:12:28 +0300

    Add celsius to kelvin

---
+++ src/battle_asserts/issues/celsius_to_kelvin.clj
@@ -0,0 +1,24 @@
+(ns battle-asserts.issues.celsius-to-kelvin
+  (:require [clojure.test.check.generators :as gen]))
+
+(def level :elementary)
+
+(def description
+  {:en "Сonvert Celsius temperature to Kelvin. Use floor rounding for the result temperature."
+   :ru "Создайте функцию, конвертирующую температуру из Цельсия в Кельвины. Полученный результат округлите в меньшую сторону."})
+
+(def signature
+  {:input [{:argument-name "temperature" :type {:name "integer"}}]
+   :output {:type {:name "integer"}}})
+
+(defn arguments-generator []
+  (gen/tuple (gen/choose -500 500)))
+
+(def test-data
+  [{:expected 308 :arguments [35]}
+   {:expected 261 :arguments [-12]}
+   {:expected 273 :arguments [0]}
+   {:expected -226 :arguments [-500]}])
+
+(defn solution [temperature]
+  (int (+ temperature 273.15)))
