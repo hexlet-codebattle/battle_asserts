ee88eec02607a1a2e51d4863a2c846aac28a17ed
solar05 <artem00298@gmail.com>
2021-03-08 02:45:26 +0300

    Add positive dominant task

---
+++ src/battle_asserts/issues/positive_dominant.clj
@@ -0,0 +1,29 @@
+(ns battle-asserts.issues.positive-dominant
+  (:require [clojure.test.check.generators :as gen]))
+
+(def level :easy)
+
+(def description
+  {:en "Create a function, thats check if unique positive numbers are greater than negative ones in `numbers` array."
+   :ru "Создайте функцию, которая проверяет, больше ли уникальных положительных чисел, чем отрицательных, в массиве `numbers`."})
+
+(def signature
+  {:input  [{:argument-name "numbers" :type {:name "array" :nested {:name "integer"}}}]
+   :output {:type {:name "boolean"}}})
+
+(defn arguments-generator []
+  (gen/tuple (gen/vector (gen/choose -200 200) 4 18)))
+
+(def test-data
+  [{:expected true :arguments [[1 2 3 -3 -4]]}
+   {:expected true :arguments [[1 1 2 2 3 3 4 -4]]}
+   {:expected false :arguments [[1 -1 -2 -3 -4]]}
+   {:expected false :arguments [[10 1 -2 -2 -3 -4]]}
+   {:expected false :arguments [[1 2 3 -3 -4 -5]]}])
+
+(defn solution [numbers]
+  (let [uniq (distinct numbers)
+        pos-numbers (filter pos? uniq)
+        neg-numbers (filter neg? uniq)]
+    (> (count pos-numbers)
+       (count neg-numbers))))
