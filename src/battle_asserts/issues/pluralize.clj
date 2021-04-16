536acab1463a94691e78397070a65a5c7ce5e52c
solar05 <artem00298@gmail.com>
2021-04-16 23:05:33 +0300

    Add pluralize task

---
+++ src/battle_asserts/issues/pluralize.clj
@@ -0,0 +1,33 @@
+(ns battle-asserts.issues.pluralize
+  (:require [clojure.test.check.generators :as gen]
+            [faker.generate :as f]))
+
+(def level :easy)
+
+(def tags ["collections" "strings"])
+
+(def description
+  {:en "Given a list of words in the singular form, return a set of those words in the plural form if they appear more than once in the list. There is no edge cases. Keep words order same."
+   :ru "В переданном списке слов найдите повторяющиеся и исправьте их на множественное число, используя обычные правила английского языка. Сохранитте порядок слов."})
+
+(def signature
+  {:input [{:argument-name "words" :type {:name "array" :nested {:name "string"}}}]
+   :output {:type {:name "array" :nested {:name "string"}}}})
+
+(defn arguments-generator []
+  (let [words (f/words {:n 25})
+        repeat-words (repeatedly 10 #(rand-nth words))
+        final-words (concat words repeat-words)]
+    (gen/tuple (gen/vector (gen/elements final-words) 3 15))))
+
+(def test-data
+  [{:expected ["cows" "pig"] :arguments [["cow" "pig" "cow" "cow"]]}
+   {:expected ["chairs"] :arguments [["chair" "chair" "chair"]]}
+   {:expected ["chair" "pencil" "arm"] :arguments [["chair" "pencil" "arm"]]}
+   {:expected ["clojure" "ruby" "rails" "elixir"] :arguments [["clojure" "ruby" "rail" "rail" "elixir"]]}])
+
+(defn solution [words]
+  (let [freq (frequencies words)
+        uniq (distinct words)]
+    (mapv (fn [word] (if (> (freq word) 1) (str word "s") word))
+          uniq)))
