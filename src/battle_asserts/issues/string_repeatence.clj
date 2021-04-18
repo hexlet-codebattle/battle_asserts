52be38cbc1ad665c4b9f1a64b3bf2aee7b50b5c2
solar05 <artem00298@gmail.com>
2021-04-18 23:17:28 +0300

    Add string repeatence task

---
+++ src/battle_asserts/issues/string_repeatence.clj
@@ -0,0 +1,32 @@
+(ns battle-asserts.issues.string-repeatence
+  (:require [clojure.test.check.generators :as gen]
+            [clojure.string :as s]
+            [faker.generate :as f]))
+
+(def level :elementary)
+
+(def tags ["training" "string"])
+
+(def description
+  {:en "Create a function that takes a string `sentence` and a number `n` and returns the repeated `sentence` an `n` number of times."
+   :ru "Создайте функцию, которая принимает строку `sentence` и число `n` и возвращает строку `sentence` повторенное `n` раз."})
+
+(def signature
+  {:input [{:argument-name "sentence" :type {:name "string"}}
+           {:argument-name "n" :type {:name "integer"}}]
+   :output {:type {:name "string"}}})
+
+(defn arguments-generator []
+  (gen/tuple (gen/elements (f/sentences {:n 30})) (gen/choose 0 10)))
+
+(def test-data
+  [{:expected "" :arguments ["Some sentence" 0]}
+   {:expected "Clojure and RubyClojure and RubyClojure and Ruby" :arguments ["Clojure and Ruby" 3]}
+   {:expected "Elixir" :arguments ["Elixir" 1]}
+   {:expected "" :arguments ["Completly new sentence" -23]}])
+
+(defn solution [sentence n]
+  (if (zero? n)
+    ""
+    (letfn [(gen [] sentence)]
+      (s/join "" (repeatedly n gen)))))
