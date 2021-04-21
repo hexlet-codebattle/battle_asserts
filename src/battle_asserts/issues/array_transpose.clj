9e2e6407109c121437988fcf68463483cc667b3d
Anton Burenkov <buran665@yandex.ru>
2021-04-21 13:58:47 +0700

    add translations

--- src/battle_asserts/issues/array_transpose.clj
+++ src/battle_asserts/issues/array_transpose.clj
@@ -5,12 +5,19 @@
 
 (def tags ["collections" "math"])
 
-(def description "Implement the matrix transposition function.
-                  Matrices are presented as arrays of arrays, where internal arrays are rows of the matrix.
-                  There are different ways to transpose a matrix:
-                  1) reflect the array over its main diagonal (which runs from top-left to bottom-right);
-                  2) write the rows of the original matrix as columns of the new one;
-                 3) write the columns of the original matrix as rows of the new one.")
+(def description
+  {:en "Implement the matrix transposition function.
+        Matrices are presented as arrays of arrays, where internal arrays are rows of the matrix.
+        There are different ways to transpose a matrix:
+        1) reflect the array over its main diagonal (which runs from top-left to bottom-right);
+        2) write the rows of the original matrix as columns of the new one;
+        3) write the columns of the original matrix as rows of the new one."
+   :ru "Реализуйте функцию транспонирования матрицы.
+        Матрицы представлены массивом массивов, где внутренние массивы - это строки матрицы.
+        Существуют различные способы транспонирования матрицы:
+        1) Отразить матрицу по главной диагонали (которая проходит от верхнего левого угла до нижнего правого);
+        2) Записать строки исходной матрицы как столбцы новой матрицы;
+        3) Записать столбцы исходной матрицы как строки новой матрицы."})
 
 (def signature
   {:input [{:argument-name "arr" :type {:name "array" :nested {:name "array" :nested {:name "integer"}}}}]
