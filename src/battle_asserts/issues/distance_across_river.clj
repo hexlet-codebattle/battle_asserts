ea37d600f4c77f304a448f948d416ecf3d07c4ac
solar05 <artem00298@gmail.com>
2021-04-22 02:31:54 +0300

    Add translations

--- src/battle_asserts/issues/distance_across_river.clj
+++ src/battle_asserts/issues/distance_across_river.clj
@@ -6,7 +6,9 @@
 
 (def tags ["math"])
 
-(def description "Compute the distance a boat travels across a river, given the width of the river, the boat's speed perpendicular to the river, and the river's speed. Use ceil rounding.")
+(def description
+  {:en "Compute the distance a boat travels across a river, given the width of the river, the boat's speed perpendicular to the river, and the river's speed. Use ceil rounding."
+   :ru "Вычислите расстояние, которое лодка проходит через реку, учитывая ширину реки, скорость лодки, перпендикулярную реке, и скорость реки."})
 
 (def signature
   {:input [{:argument-name "width" :type {:name "integer"}}
--- src/battle_asserts/issues/fps_calculation.clj
+++ src/battle_asserts/issues/fps_calculation.clj
@@ -3,8 +3,9 @@
 
 (def level :elementary)
 
-(def description "Create a function that returns the number of frames shown in a given number of minutes for a certain FPS.
-                  Note: FPS stands for \"frames per second\" and it's the number of frames a computer screen shows every second.")
+(def description
+  {:en "Create a function that returns the number of frames shown in a given number of minutes for a certain FPS. Note: FPS stands for \"frames per second\" and it's the number of frames a computer screen shows every second."
+   :ru "Создайте функцию, которая рассчитывает количество кадров, показанных в заданном количестве минут для определенного FPS. Примечание: FPS означает \"количество кадров в секунду\" и это количество кадров, показываемых каждую секунду на экране компьютера."})
 
 (def signature
   {:input [{:argument-name "minutes" :type {:name "integer"}}
