a268b968a9cf11cf0e9eec37174772e02d66c205
solar05 <artem00298@gmail.com>
2021-04-22 23:50:10 +0300

    Add translations

--- src/battle_asserts/issues/circle_point.clj
+++ src/battle_asserts/issues/circle_point.clj
@@ -5,9 +5,9 @@
 
 (def tags ["math"])
 
-(def description "On the flat there is a circle centered at zero point (0, 0)
-                  with radius r. Does the point with coordinates x and y belongs to this circle.
-                  Tip: to solve the problem, you need to find the hypotenuse.")
+(def description
+  {:en "On the flat there is a circle centered at zero point (0, 0) with radius r. Does the point with coordinates x and y belongs to this circle. Tip: to solve the problem, you need to find the hypotenuse."
+   :ru "Дана точка с координатами `x` и `y` и окружность с радиусом `r` в точке (0, 0). Проверьте, находится ли точка в окружности."})
 
 (def signature
   {:input  [{:argument-name "x" :type {:name "integer"}}
