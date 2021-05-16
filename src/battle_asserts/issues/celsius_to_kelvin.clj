42dee93979a33aa3b1959064826390c225aee42e
Artem Solomatin <artem00298@gmail.com>
2021-05-16 21:54:25 +0300

    Update celsius_to_kelvin.clj (#769)
    * Update celsius_to_kelvin.clj
    
    * Update celsius_to_kelvin.clj

--- src/battle_asserts/issues/celsius_to_kelvin.clj
+++ src/battle_asserts/issues/celsius_to_kelvin.clj
@@ -6,8 +6,8 @@
 (def tags ["conversion" "physics"])
 
 (def description
-  {:en "Сonvert Celsius temperature to Kelvin. Use floor rounding for the result temperature."
-   :ru "Создайте функцию, конвертирующую температуру из Цельсия в Кельвины. Полученный результат округлите в меньшую сторону."})
+  {:en "Сonvert Celsius temperature to Kelvin. Consider using 273 for conversion."
+   :ru "Создайте функцию, конвертирующую температуру из Цельсия в Кельвины. Используйте при конвертации число 273."})
 
 (def signature
   {:input [{:argument-name "temperature" :type {:name "integer"}}]
@@ -20,7 +20,7 @@
   [{:expected 308 :arguments [35]}
    {:expected 261 :arguments [-12]}
    {:expected 273 :arguments [0]}
-   {:expected -226 :arguments [-500]}])
+   {:expected -227 :arguments [-500]}])
 
 (defn solution [temperature]
-  (int (+ temperature 273.15)))
+  (+ temperature 273))
