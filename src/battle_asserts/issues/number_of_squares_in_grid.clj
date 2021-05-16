1ffe1ffd84246fff42cef9f7c2e4d9fd5c8c4dfd
Anton Burenkov <buran665@yandex.ru>
2021-05-16 18:18:08 +0700

    fix typo

--- src/battle_asserts/issues/number_of_squares_in_grid.clj
+++ src/battle_asserts/issues/number_of_squares_in_grid.clj
@@ -7,7 +7,7 @@
 
 (def description
   {:en "Create a function that calculates the number of different squares in an `n * n` square grid."
-   :ru "Создайте функцию, которая подсчитывает число различных квадртаров на квадратной сетке размера `n * n`."})
+   :ru "Создайте функцию, которая подсчитывает число различных квадратов на квадратной сетке размера `n * n`."})
 
 (def signature
   {:input [{:argument-name "number" :type {:name "integer"}}]
