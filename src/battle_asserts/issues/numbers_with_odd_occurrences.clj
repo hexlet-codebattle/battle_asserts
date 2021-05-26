2e7e1d681de8769bec9c16633ebbfc195d7ad9fa
Safintim <timurtlt96@mail.ru>
2021-05-26 11:34:19 +0300

    Fixed translate

--- src/battle_asserts/issues/numbers_with_odd_occurrences.clj
+++ src/battle_asserts/issues/numbers_with_odd_occurrences.clj
@@ -7,7 +7,7 @@
 
 (def description
   {:en "Find numbers which have odd occurrences in array."
-   :ru "Найдите числа, которые встречаются четное количество раз в массиве."})
+   :ru "Найдите числа, которые встречаются нечетное количество раз в массиве."})
 
 (def signature
   {:input  [{:argument-name "arr" :type {:name "array" :nested {:name "integer"}}}]
