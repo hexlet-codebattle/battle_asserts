173be3d29213364bbb45254b6852621ae3715163
Anton Burenkov <buran665@yandex.ru>
2021-06-02 19:36:26 +0700

    add translations

--- src/battle_asserts/issues/triangular_number.clj
+++ src/battle_asserts/issues/triangular_number.clj
@@ -5,10 +5,15 @@
 
 (def tags ["math"])
 
-(def description "Triangular Number Sequence is generated from a pattern of dots that form a triangle.
-                  The first 5 numbers of the sequence, or dots, are: `1, 3, 6, 10, 15`
-                  This means that the first triangle has just one dot, the second one has three dots, the third one has 6 dots and so on.
-                  Write a function that gives the number of dots with its corresponding triangle number of the sequence.")
+(def description
+  {:en "Triangular Number Sequence is generated from a pattern of dots that form a triangle.
+        The first 5 numbers of the sequence, or dots, are: `1, 3, 6, 10, 15`
+        This means that the first triangle has just one dot, the second one has three dots, the third one has 6 dots and so on.
+        Write a function that gives the number of dots with its corresponding triangle number of the sequence."
+   :ru "Последовательность треугольных чисел генерируется из шаблона точек образующих треугольник.
+        Первые 5 чисел последовательности, или точек - это `1, 3, 6, 10, 15`.
+        Это значит что первый треугольник имеет всего одну точку, второй - три точки, третий - 6 и так далее.
+        Напишите функцию, которая принимает порядковый номер треугольного числа и возвращает соответствующее ему количество точек."})
 
 (def signature
   {:input [{:argument-name "n" :type {:name "integer"}}]
