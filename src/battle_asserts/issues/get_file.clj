d31e78c1dbb6f7559cd3feafedbc4dc53825883f
solar05 <artem00298@gmail.com>
2021-07-04 16:39:57 +0300

    Add more tags

--- src/battle_asserts/issues/get_file.clj
+++ src/battle_asserts/issues/get_file.clj
@@ -5,7 +5,7 @@
 
 (def level :elementary)
 
-(def tags ["training" "strings"])
+(def tags ["training" "strings" "os"])
 
 (def description
   {:en "Implement a function that returns the filename with extension from a given path."
