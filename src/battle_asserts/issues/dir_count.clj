112cc87305342e2cc05b29e49df3e3e7eb1bdd81
solar05 <artem00298@gmail.com>
2021-07-04 16:27:55 +0300

    Fix indents

--- src/battle_asserts/issues/dir_count.clj
+++ src/battle_asserts/issues/dir_count.clj
@@ -19,7 +19,7 @@
   (letfn [(words-count [] (gen/generate (gen/choose 2 8)))
           (extension [] (gen/generate (gen/elements [".txt" ".exe" ".mp3" ".docx" ".jpg" ".png" ".pdf" ".sh" ".rb" ".ex" ".erl" ".js" ".html"])))
           (filepath []
-            (repeatedly 10 #(str (rand-nth ["C:/" "D:/" "F:/" "/"]) (s/join "/" (faker/words {:lang :en :n (words-count)})) (rand-nth [(extension) ""]) )))]
+            (repeatedly 10 #(str (rand-nth ["C:/" "D:/" "F:/" "/"]) (s/join "/" (faker/words {:lang :en :n (words-count)})) (rand-nth [(extension) ""]))))]
     (gen/tuple (gen/vector (gen/elements (filepath)) 2 15))))
 
 (def test-data
