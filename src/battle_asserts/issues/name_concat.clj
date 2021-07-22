58c2e0ee87dc6dd23aaad75d8f61c7855faf965e
Artem Solomatin <artem00298@gmail.com>
2021-07-22 23:32:48 +0300

    Add name concat task (#812)
    * Add name concat task
    
    * Fix lint

---
+++ src/battle_asserts/issues/name_concat.clj
@@ -0,0 +1,32 @@
+(ns battle-asserts.issues.name-concat
+  (:require [clojure.string :as s]
+            [clojure.test.check.generators :as gen]
+            [faker.name :as fk]))
+
+(def level :elementary)
+
+(def tags ["training" "strings" "collections" "hash-maps"])
+
+(def description
+  {:en "Implement a function that combine first and last name from hash into full name."
+   :ru "Создайте функцию, которая извлекает из хеша имя и фамилию и собирает их в одну строку."})
+
+(def signature
+  {:input [{:argument-name "names" :type {:name "array" :nested {:name "hash" :nested {:name "string"}}}}]
+   :output {:type {:name "array" :nested {:name "string"}}}})
+
+(defn arguments-generator []
+  (let [first-names (repeatedly 40 fk/first-name)
+        last-names  (repeatedly 40 fk/last-name)
+        names (repeatedly 50 (fn [] {:first (rand-nth first-names) :last (rand-nth last-names)}))]
+    (gen/tuple (gen/vector (gen/elements names) 3 10))))
+
+(def test-data
+  [{:expected ["John Doe" "Rich Hickey"]
+    :arguments [[{:first "John" :last "Doe"} {:first "Rich" :last "Hickey"}]]}
+   {:expected ["Harold Abelson" "Gerald Jay Sussman" "Pat Shaughnessy" "Matthias Felleisen"]
+    :arguments [[{:first "Harold" :last "Abelson"} {:first "Gerald" :last "Jay Sussman"} {:first "Pat" :last "Shaughnessy"} {:first "Matthias" :last "Felleisen"}]]}])
+
+(defn solution [names]
+  (mapv (fn [elem] (s/join #" " [(elem :first) (elem :last)])) names))
+
