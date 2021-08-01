c05cc8bb5dc0c2d3b42651c1b89b8e6cfb1cb240
solar05 <artem00298@gmail.com>
2021-08-01 23:02:40 +0300

    Migrate between text task

---
+++ src/battle_asserts/issues/between_text.clj
@@ -0,0 +1,40 @@
+(ns battle-asserts.issues.between-text
+  (:require [clojure.test.check.generators :as gen]
+            [faker.generate :as fk]
+            [clojure.string :as s]))
+
+(def level :easy)
+
+(def tags ["training" "strings"])
+
+(def description
+  {:en "Write function that returns subtext from text between given patterns."
+   :ru "Напишите функцию, которая возвращает подтекст из текста между заданным шаблоном."})
+
+(def signature
+  {:input [{:argument-name "str" :type {:name "string"}}
+           {:argument-name "left" :type {:name "string"}}
+           {:argument-name "right" :type {:name "string"}}]
+   :output {:type {:name "string"}}})
+
+(defn arguments-generator []
+  (let [variants (repeatedly 2 #(fk/sentence {:words-range [4 4]}))
+        left-variants (mapv #(rand-nth (first (partition 2 (s/split % #" ")))) variants)
+        right-variants (mapv #(rand-nth (last (partition 2 (s/split % #" ")))) variants)]
+    (gen/tuple (gen/elements variants) (gen/elements left-variants) (gen/elements right-variants))))
+
+(def test-data
+  [{:expected "foo" :arguments ["<a>foo</a>" "<a>" "</a>"]}
+   {:expected "text" :arguments ["this is 'text'" "'" "'"]}
+   {:expected "oni" :arguments ["xonix" "x" "x"]}
+   {:expected "google" :arguments ["www.google.com" "www." ".com"]}
+   {:expected " wow " :arguments ["oh wow such example" "oh" "such"]}])
+
+(defn solution [string left right]
+  (let [result (-> (str left "(.*)" right)
+                   re-pattern
+                   (re-find string)
+                   rest)]
+    (if (empty? result)
+      ""
+      (first result))))
