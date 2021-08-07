df5ca1eabe1d256587c1075970d9f4b06b689130
solar05 <artem00298@gmail.com>
2021-08-07 03:00:21 +0300

    Refactor broadcast finder

--- src/battle_asserts/issues/broadcast_finder.clj
+++ src/battle_asserts/issues/broadcast_finder.clj
@@ -8,23 +8,24 @@
 (def tags ["network" "strings"])
 
 (def description
-  {:en "Create a function, thats finds broadcast network address in `address`, which contains IPv4 address and subnet mask."
-   :ru "Создайте функцию, которая находит адрес широкого вещания, входящий в `address`, который содержит IPv4 адрес и маску подсети."})
+  {:en "Create a function, thats finds broadcast network address in `address`, which contains IPv4 address and subnet mask. If adrerss invalid, return \"invalid adress!!111\""
+   :ru "Создайте функцию, которая находит адрес широкого вещания, входящий в `address`, который содержит IPv4 адрес и маску подсети. Если адрес невалиден, то верните \"invalid adress!!111\""})
 
 (def signature
   {:input  [{:argument-name "address" :type {:name "string"}}]
    :output {:type {:name "string"}}})
 
+(defn- gen-ip-address []
+  (str (s/join #"."
+               (gen/generate
+                (gen/vector
+                 (gen/choose 0 255) 4)))
+       "/"
+       (gen/generate (gen/choose 22 32))))
+
 (defn arguments-generator []
-  (letfn [(ip []
-            (s/join #"/"
-                    [(s/join #"."
-                             (apply conj
-                                    (gen/generate (gen/vector (gen/choose 1 255) 1))
-                                    (gen/generate (gen/vector (gen/choose 0 255) 3))))
-                     (gen/generate (gen/choose 12 30))]))
-          (gen-ip [] (gen/elements (repeatedly 20 ip)))]
-    (gen/tuple (gen-ip))))
+  (let [addresses (repeatedly 30 gen-ip-address)]
+    (gen/tuple (gen/elements addresses))))
 
 (def test-data
   [{:expected "192.175.255.255"
@@ -32,7 +33,11 @@
    {:expected "198.201.255.255"
     :arguments ["198.201.121.1/15"]}
    {:expected "201.224.121.15"
-    :arguments ["201.224.121.12/30"]}])
+    :arguments ["201.224.121.12/30"]}
+   {:expected "invalid adress!!111"
+    :arguments ["0.168.100.1/33"]}])
 
 (defn solution [address]
-  (str (last (ip/make-network address))))
+  (try
+    (str (last (ip/make-network address)))
+    (catch java.lang.AssertionError _ "invalid adress!!111")))
