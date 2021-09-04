327e1c70a0079e80be80b16c0d9689484695c571
solar05 <artem00298@gmail.com>
2021-09-05 01:56:14 +0300

    Add path to docker task

--- src/battle_asserts/issues/docker_builder.clj
+++ src/battle_asserts/issues/docker_builder.clj
@@ -1,18 +1,20 @@
 (ns battle-asserts.issues.docker-builder
   (:require [clojure.string :as s]
-            [clojure.test.check.generators :as gen]))
+            [clojure.test.check.generators :as gen]
+            [faker.generate :as fk]))
 
 (def level :easy)
 
 (def tags ["strings" "devops" "codebattle"])
 
 (def description
-  {:en "Create a function, that create docker build command from `language` and `version` for codebattle."
-   :ru "Создайте функцию, которая генерирует команду сборки docker образа для codebattle с помощью `language` и `version`."})
+  {:en "Create a function, that create docker build command from `language`, `version` and `path` (which can be empty string) for codebattle."
+   :ru "Создайте функцию, которая генерирует команду сборки docker образа для codebattle с помощью `language`, `version` и `path` (который может быть пустой стрококй)."})
 
 (def signature
   {:input [{:argument-name "language" :type {:name "string"}}
-           {:argument-name "version" :type {:name "string"}}]
+           {:argument-name "version" :type {:name "string"}}
+           {:argument-name "path" :type {:name "string"}}]
    :output {:type {:name "string"}}})
 
 (defn- gen-version []
@@ -21,18 +23,28 @@
         release (rand-nth ["slim" "buster" "alpine" "cli" "latest"])]
     (str version ":" release)))
 
+(defn- gen-path []
+  (let [elems (fk/words {:n (gen/generate (gen/choose 1 8))})
+        combined (concat (conj elems ".") '("Dockerfile"))]
+  (s/join #"/" combined)))
+
 (defn arguments-generator []
   (let [languages ["elixir" "ruby" "haskell" "c++" "c#" "clojure" "dart" "go" "java" "js" "kotlin" "php" "python"]
-        versions (repeatedly 40 gen-version)]
-    (gen/tuple (gen/elements languages) (gen/elements versions))))
+        versions (repeatedly 40 gen-version)
+        dirs (conj (repeatedly 30 gen-path) "")]
+    (gen/tuple (gen/elements languages)
+               (gen/elements versions)
+               (gen/elements dirs))))
 
 (def test-data
-  [{:arguments ["clojure" "1.1.2:slim"]
+  [{:arguments ["clojure" "1.1.2:slim" ""]
     :expected  "docker build -t codebattle/clojure:1.1.2:slim ."}
-   {:arguments ["ruby" "3.0.2:latest"]
+   {:arguments ["ruby" "3.0.2:latest" ""]
     :expected  "docker build -t codebattle/ruby:3.0.2:latest ."}
-   {:arguments ["elixir" "1.12.2:alpine"]
-    :expected  "docker build -t codebattle/elixir:1.12.2:alpine ."}])
+   {:arguments ["elixir" "1.12.2:alpine" "./Some/Dockerfile"]
+    :expected  "docker build -t codebattle/elixir:1.12.2:alpine -f ./Some/Dockerfile ."}])
 
-(defn solution [language version]
-  (str "docker build -t codebattle/" language ":" version " ."))
+(defn solution [language version path]
+  (str "docker build -t codebattle/" language ":" version
+       (when-not (empty? path) (str " -f " path))
+       " ."))
