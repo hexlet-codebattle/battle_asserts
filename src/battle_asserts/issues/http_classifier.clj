3e107660f086d942df45d0b584e7d6d402e5b184
Artem Solomatin <artem00298@gmail.com>
2021-10-02 03:13:54 +0300

    Add http-classifier task (#861)

---
+++ src/battle_asserts/issues/http_classifier.clj
@@ -0,0 +1,37 @@
+(ns battle-asserts.issues.http-classifier
+  (:require [clojure.test.check.generators :as gen]
+            [clojure.spec.alpha :as s]))
+
+(def level :elementary)
+
+(def tags ["training" "strings" "network"])
+
+(def description
+  {:en "Return type of provided http status `code`. Checkout examples for types."
+   :ru "Верните тип полученного http `code`. В примерах приведены возможные типы кодов."})
+
+(def signature
+  {:input [{:argument-name "code" :type {:name "integer"}}]
+   :output {:type {:name "string"}}})
+
+(defn arguments-generator []
+  (gen/tuple (gen/one-of [(gen/choose 100 199)
+                          (gen/choose 200 299)
+                          (gen/choose 300 399)
+                          (gen/choose 400 499)
+                          (gen/choose 500 599)])))
+
+(def test-data
+  [{:expected "informational" :arguments [121]}
+   {:expected "success" :arguments [201]}
+   {:expected "redirection" :arguments [333]}
+   {:expected "client error" :arguments [418]}
+   {:expected "server error" :arguments [500]}])
+
+(defn solution [code]
+  (cond
+    (s/int-in-range? 100 199 code) "informational"
+    (s/int-in-range? 200 299 code) "success"
+    (s/int-in-range? 300 399 code) "redirection"
+    (s/int-in-range? 400 499 code) "client error"
+    (s/int-in-range? 500 599 code) "server error"))
