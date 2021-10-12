77dc26d5b5308640cd3a6531a6d6b7efd75eec0b
Jeff Parker <jeffp42ker@gmail.com>
2021-10-11 22:54:52 -0400

    garden-help issue: rephrased description (English only); added arguments-generator and solution, modified test-data.

--- src/battle_asserts/issues/garden_help.clj
+++ src/battle_asserts/issues/garden_help.clj
@@ -1,13 +1,18 @@
-(ns battle-asserts.issues.garden-help)
+(ns battle-asserts.issues.garden-help
+  (:require [clojure.test.check.generators :as gen]))
 
 (def level :medium)
 
 (def tags ["collections" "math"])
 
-(def disabled true)
-
 (def description
-  {:en "Yaroslav helps his grandmother plant carrots in the seedbed. The seedbed that Yaroslav is planting is an infinite line. At each integer coordinate you can plant a carrot. Grandmother has already planted `N` carrots in `x` cells. However, grandmother thinks she hasn't planted carrots densely enough, and asks Yaroslav to count how many more cells can be planted. Carrots have a peculiarity - they do not grow if they are planted too sparsely. Grandmother wants the distance from the additional planting to the sown cell to be equal to `distance'. Grandmother realizes that there are infinitely many planting options, so she asks Yaroslav to count only the cells between other planted cells. Yaroslav's grandmother is frugal and does not plant more than one carrot per cell. Create a fuction that takes an array of `cells` from an increasing sequence of `X,, X,,, ... X, `n` numbers, `X`,- coordinate of the seeded cell and `distance` - the minimum distance the grandmother expects. We need to find the number of possible seedings."
+  {:en "Yaroslav helps his grandmother plant carrots in a single-line seedbed where a carrot can be planted at each
+  integer coordinate along the line. Having already planted carrots in cells spaced at various distances along the line,
+  Grandmother has learned carrots do not grow well when planted too sparsely and thinks the carrots need to be planted
+  more densely to grow. She asks Yaroslav to count how many additional cells can be planted with carrots between the
+  current planted cells, given a minimum `distance` between planted cells.
+  Create a function that returns the number of additional cells that can be seeded given 1) an integer array of seeded `cells` in an
+  increasing sequence of `X,, X,,, ... X,` and 2) a minimum `distance` grandmother expects between planted cells."
    :ru "Ярослав помогает бабушке садить морковку на грядке. Грядка, которую засевает Ярослав, представляет собой бесконечную прямую. В каждой целочисленной координате можно посадить морковку. Бабушка уже посадила `N` морковок в `x` ячеек. Однако бабушка считает, что недостаточно плотно засеяла морковку, и просит Ярослава посчитать сколько ячеек возможно еще засеять. У морковки есть особенность, она не растет, если посажена слишком редко. Бабушка хочет, чтобы расстояние от дополнительной посадки до засеянной ячейки было равно `distance`. Бабушка понимает, что вариантов посадки бесконечно много, поэтому просит Ярослава посчитать только ячейки между другими засеянными ячейками. Бабушка у Ярослава экономная и не садит более одной морковки в ячейку. Создайте фугкцию, которая принимает массив `cells` из возрастающей последовательности `Х,, Х,,, ... Х`, `n` чисел, `Х`,- координата засеянной ячейки и `distance` - минимальное расстояние которое ожидает бабушка. Нужно найти количество возможных вариантов посадки."})
 
 (def signature
@@ -15,6 +20,26 @@
             {:argument-name "distance" :type {:name "integer"}}]
    :output {:type {:name "integer"}}})
 
+(defn arguments-generator []
+  (gen/tuple (gen/fmap (comp vec sort)
+                       (gen/vector-distinct gen/small-integer
+                                            {:max-elements 15 :min-elements 5}))
+             (gen/elements (range 2 5))))
+
 (def test-data
-  [{:expected 2 :arguments [[4 8 11 18 19] 2]}
-   {:expected 4 :arguments [[-3 2 9 16] 3]}])
+  [{:expected 3 :arguments [[4 8 11 18 19] 2]}
+   {:expected 2 :arguments [[-3 2 9 16] 3]}
+   {:expected 5 :arguments [[-28 -26 -23 -16 -12 -10 4 8 10 11 13 16 17 23] 3]}
+   {:expected 13 :arguments [[-27 -24 -23 -18 -14 -11 2 5 6 7 16 20 22 28] 2]}])
+
+(defn solution [cells distance]
+  (loop [idx 0 offset 0 seedings 0]
+    (if (= idx (dec (count cells)))
+      seedings
+      (let [a (nth cells idx)
+            offset (+ offset distance)
+            b (nth cells (inc idx))]
+        (cond (and (< a (+ a offset) b)
+                   (<= distance (- b (+ a offset))))
+              (recur idx offset (inc seedings))
+              :else (recur (inc idx) 0 seedings))))))
