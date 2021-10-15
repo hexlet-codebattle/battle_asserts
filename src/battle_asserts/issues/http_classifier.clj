5b387945b0c243523033128b2046031516566a52
Artem Solomatin <artem00298@gmail.com>
2021-10-16 00:51:41 +0300

    Fix task blinking for CI (#866)

--- src/battle_asserts/issues/http_classifier.clj
+++ src/battle_asserts/issues/http_classifier.clj
@@ -7,15 +7,16 @@
 (def tags ["training" "strings" "network"])
 
 (def description
-  {:en "Return type of provided http status `code`. Checkout examples for types."
-   :ru "Верните тип полученного http `code`. В примерах приведены возможные типы кодов."})
+  {:en "Return type of provided http status `code`. Checkout examples for types. If code has no classification, return \"unrecognized\""
+   :ru "Верните тип полученного http `code`. В примерах приведены возможные типы кодов. Если код не поддается классификации, то верните \"unrecognized\""})
 
 (def signature
   {:input [{:argument-name "code" :type {:name "integer"}}]
    :output {:type {:name "string"}}})
 
 (defn arguments-generator []
-  (gen/tuple (gen/one-of [(gen/choose 100 199)
+  (gen/tuple (gen/one-of [(gen/choose 0 1000)
+                          (gen/choose 100 199)
                           (gen/choose 200 299)
                           (gen/choose 300 399)
                           (gen/choose 400 499)
@@ -34,4 +35,5 @@
     (s/int-in-range? 200 299 code) "success"
     (s/int-in-range? 300 399 code) "redirection"
     (s/int-in-range? 400 499 code) "client error"
-    (s/int-in-range? 500 599 code) "server error"))
+    (s/int-in-range? 500 599 code) "server error"
+    :else "unrecognized"))
