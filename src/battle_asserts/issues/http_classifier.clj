46012015883d1689ccfad6979610bdb96a7f5292
solar05 <artem00298@gmail.com>
2021-11-09 21:45:21 +0300

    Fix http classifier task

--- src/battle_asserts/issues/http_classifier.clj
+++ src/battle_asserts/issues/http_classifier.clj
@@ -31,9 +31,9 @@
 
 (defn solution [code]
   (cond
-    (s/int-in-range? 100 199 code) "informational"
-    (s/int-in-range? 200 299 code) "success"
-    (s/int-in-range? 300 399 code) "redirection"
-    (s/int-in-range? 400 499 code) "client error"
-    (s/int-in-range? 500 599 code) "server error"
+    (s/int-in-range? 100 200 code) "informational"
+    (s/int-in-range? 200 300 code) "success"
+    (s/int-in-range? 300 400 code) "redirection"
+    (s/int-in-range? 400 500 code) "client error"
+    (s/int-in-range? 500 600 code) "server error"
     :else "unrecognized"))
