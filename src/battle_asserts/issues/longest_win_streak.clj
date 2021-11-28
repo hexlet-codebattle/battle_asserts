73c884e0ef0c0595823613ebbd060fcf8420486d
sgmdlt <69588756+sgmdlt@users.noreply.github.com>
2021-11-29 02:11:47 +0300

    add longest_win_streak issue (#883)

---
+++ src/battle_asserts/issues/longest_win_streak.clj
@@ -0,0 +1,38 @@
+(ns battle-asserts.issues.longest-win-streak
+  (:require [clojure.test.check.generators :as gen]
+            [faker.name :as fk-name]))
+
+(def level :easy)
+
+(def tags ["collections" "strings"])
+
+(def description
+  {:en "Two friends play Codebattle and record the winner of each round.
+        Find the name of the player with the longest winning streak.
+        If the winning series is equal, return Draw!."
+   :ru "Двое друзей играют в Codebattle и записывают победителя каждого раунда.
+        Найдите имя игрока с самой длинной выигрышной серией.
+        Если серии побед равны, верните Draw!."})
+
+(def signature
+  {:input [{:argument-name "winners" :type {:name "array" :nested {:name "string"}}}]
+   :output {:type {:name "string"}}})
+
+(defn arguments-generator []
+  (gen/tuple (gen/vector (gen/elements (repeatedly 2 #(fk-name/first-name))) 4 10)))
+
+(def test-data
+  [{:expected "Alice"
+    :arguments [["Bob" "Bob" "Alice" "Alice" "Alice" "Bob" "Bob" "Alice"]]}
+   {:expected "Bob"
+    :arguments [["Bob" "Alice" "Bob" "Bob"]]}
+   {:expected "Draw!"
+    :arguments [["Bob" "Alice" "Alice" "Bob" "Bob"]]}])
+
+(defn solution [winners]
+  (let [win-streaks (partition-by identity winners)
+        maximum (apply max (map count win-streaks))
+        max-streaks (filter #(= maximum (count %)) win-streaks)]
+    (if (> (count max-streaks) 1)
+      "Draw!"
+      (first (flatten max-streaks)))))
