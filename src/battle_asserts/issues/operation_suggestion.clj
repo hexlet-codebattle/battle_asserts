c351f854e5afe9c3be81b3f294d723b878ca5ba1
Artem Solomatin <artem00298@gmail.com>
2021-12-02 15:33:50 +0300

    Remove division from task

--- src/battle_asserts/issues/operation_suggestion.clj
+++ src/battle_asserts/issues/operation_suggestion.clj
@@ -6,8 +6,8 @@
 (def tags ["games" "math"])
 
 (def description
-  {:en "Write a function that takes three numbers (first operand, second operand and result) and return string if they should be added, subtracted, multiplied or divided to get result."
-   :ru "Создайте функцию, которая принимает три числа (первый операнд, второй операнд и результат) и возвращает строку, если они должны быть добавлены, вычтены, умножены или разделены для получения результата. Результирующая операция должна быть на английском языке."})
+  {:en "Write a function that takes three numbers (first operand, second operand and result) and return string if they should be added, subtracted or multiplied to get result."
+   :ru "Создайте функцию, которая принимает три числа (первый операнд, второй операнд и результат) и возвращает строку, если они должны быть добавлены, вычтены или умножены для получения результата. Результирующая операция должна быть на английском языке."})
 
 (def signature
   {:input [{:argument-name "first" :type {:name "integer"}}
@@ -18,13 +18,12 @@
 (def operations-map
   {"+" +
    "-" -
-   "*" *
-   "/" (fn [a b] (/ a b))})
+   "*" *})
 
 (defn arguments-generator []
   (let [first-num (gen/generate (gen/choose -50 50))
         second-num (gen/generate (gen/one-of [(gen/choose -50 -1) (gen/choose 1 50)]))
-        result [(+ first-num second-num) (- first-num second-num) (* first-num second-num) (/ first-num second-num) second-num]]
+        result [(+ first-num second-num) (- first-num second-num) (* first-num second-num) second-num]]
     (gen/tuple (gen/return first-num) (gen/return second-num) (gen/elements result))))
 
 (def test-data
@@ -34,8 +33,6 @@
     :expected "substraction"}
    {:arguments [10 2 20]
     :expected "multiply"}
-   {:arguments [33 3 11]
-    :expected "division"}
    {:arguments [33 33 33]
     :expected "unrecognized operation!!"}])
 
@@ -44,5 +41,4 @@
     (= ((operations-map "+") first-num second-num) result) "addition"
     (= ((operations-map "-") first-num second-num) result) "substraction"
     (= ((operations-map "*") first-num second-num) result) "multiply"
-    (= ((operations-map "/") first-num second-num) result) "division"
     :else "unrecognized operation!!"))
