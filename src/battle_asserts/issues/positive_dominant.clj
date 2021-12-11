820f7ab2c95346609a0cb9eb7db52472aa9a2b94
sgmdlt <69588756+sgmdlt@users.noreply.github.com>
2021-12-11 17:25:27 +0300

    fix issue (#898)

--- src/battle_asserts/issues/positive_dominant.clj
+++ src/battle_asserts/issues/positive_dominant.clj
@@ -6,19 +6,19 @@
 (def tags ["collections"])
 
 (def description
-  {:en "Create a function, thats check if unique positive numbers are greater than negative ones in `numbers` array."
-   :ru "Создайте функцию, которая проверяет, больше ли уникальных положительных чисел, чем отрицательных, в массиве `numbers`."})
+  {:en "Create a function, thats check if positive numbers are greater than negative ones in `numbers` array after removing duplicates."
+   :ru "Создайте функцию, которая проверяет, больше ли положительных чисел, чем отрицательных, в массиве `numbers` после удаления дубликатов."})
 
 (def signature
   {:input  [{:argument-name "numbers" :type {:name "array" :nested {:name "integer"}}}]
    :output {:type {:name "boolean"}}})
 
 (defn arguments-generator []
-  (gen/tuple (gen/vector (gen/choose -200 200) 4 18)))
+  (gen/tuple (gen/vector (gen/choose -5 5) 10 18)))
 
 (def test-data
   [{:expected true :arguments [[1 2 3 -3 -4]]}
-   {:expected true :arguments [[1 1 2 2 3 3 4 -4]]}
+   {:expected true :arguments [[1 1 2 2 3 3 -4 -4]]}
    {:expected false :arguments [[1 -1 -2 -3 -4]]}
    {:expected false :arguments [[10 1 -2 -2 -3 -4]]}
    {:expected false :arguments [[1 2 3 -3 -4 -5]]}])
