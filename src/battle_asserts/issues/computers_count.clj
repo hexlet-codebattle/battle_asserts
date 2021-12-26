df61e9c68afcaf5ec7cc9a841e92016791bf70ab
solar05 <artem00298@gmail.com>
2021-12-27 00:44:50 +0300

    Update description

--- src/battle_asserts/issues/computers_count.clj
+++ src/battle_asserts/issues/computers_count.clj
@@ -8,7 +8,7 @@
 
 (def description
   {:en "Write a method/function that takes a number (int) as input, and outputs a word in the plural or singular (only simple cases) corresponding to the specified number."
-   :ru "Написать метод/функцию, который/которая на вход принимает число (int), а на выходе выдает слово в множественном или единственном числе (только простые случаи), соответствующем указанному количеству."})
+   :ru "Написать метод/функцию, который/которая на вход принимает число (int), а на выходе выдает слово в множественном или единственном числе (только простые случаи), соответствующем указанному количеству (на английском языке)."})
 
 (def signature
   {:input [{:argument-name "number" :type {:name "integer"}}]
