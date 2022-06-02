913ab0cae01ccc9e75b23e1865dc57ec5d4da9cb
Artem Solomatin <artem00298@gmail.com>
2022-06-03 00:41:33 +0300

    Bump deps and update dir count (#938)

--- src/battle_asserts/issues/dir_count.clj
+++ src/battle_asserts/issues/dir_count.clj
@@ -16,11 +16,11 @@
    :output {:type {:name "integer"}}})
 
 (defn arguments-generator []
-  (letfn [(words-count [] (gen/generate (gen/choose 2 8)))
+  (letfn [(words-count [] (gen/generate (gen/choose 2 6)))
           (extension [] (gen/generate (gen/elements [".txt" ".exe" ".mp3" ".docx" ".jpg" ".png" ".pdf" ".sh" ".rb" ".ex" ".erl" ".js" ".html"])))
           (filepath []
-            (repeatedly 10 #(str (rand-nth ["C:/" "D:/" "F:/" "/"]) (s/join "/" (faker/words {:lang :en :n (words-count)})) (rand-nth [(extension) ""]))))]
-    (gen/tuple (gen/vector (gen/elements (filepath)) 2 15))))
+            (repeatedly 15 #(str (rand-nth ["C:/" "D:/" "F:/" "/"]) (s/join "/" (faker/words {:lang :en :n (words-count)})) (rand-nth [(extension) ""]))))]
+    (gen/tuple (gen/vector (gen/elements (filepath)) 2 20))))
 
 (def test-data
   [{:expected 1
