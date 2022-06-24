fc260b6b21b13fcf6f53d7d77c441176f380d5c4
solar05 <artem00298@gmail.com>
2022-06-24 13:44:36 +0300

    Update generator tool

--- src/battle_asserts/utility_tools.clj
+++ src/battle_asserts/utility_tools.clj
@@ -128,13 +128,10 @@
       (println "There is no disabled tasks! Yaay!")
       (println (str "Disabled tasks list:\n" (s/join ", " @disabled-list) "\nTotal number of disabled tasks is: " (count @disabled-list) "!")))))
 
-(defn generate-isssue-template
-  "Tool that generates issue template with hints"
-  [& _args]
-  (let [issue-file (io/as-file "./src/battle_asserts/issues/issue_template.clj")
-        issue-template "; FIXME rename file and namespace to actual issue name (required, check CONTRIBUTING.md)
-(ns battle-asserts.issues.issue-template
-  (:require [clojure.test.check.generators :as gen]))
+(defn- generate-isssue-template [issue-name]
+  (let [issue-filename (s/replace issue-name #" " "_")
+        issue-file (io/as-file (str "./src/battle_asserts/issues/" issue-filename ".clj"))
+        issue-body-template "  (:require [clojure.test.check.generators :as gen]))
         
 ; FIXME add actual task level (required)
 (def level :elementary)
@@ -155,9 +152,20 @@
 
 ; FIXME add actual solution for issue (optional, you can delete solution)
 (defn solution [args])
-"]
+"
+        issue-ns (s/replace issue-name #" " "-")
+        issue-template (str "(ns battle-asserts.issues." issue-ns "\n" issue-body-template)]
     (if (.exists issue-file)
       (println "Issue template already exist!")
       (do
         (spit issue-file issue-template)
-        (println "Issue template generated, checkout file src/battle_asserts/issues/issue_template.clj")))))
+        (println (str "Issue template generated, checkout file " (.getAbsolutePath issue-file)))))))
+
+(defn run-isssue-template-generator
+  "Tool that generates issue template with hints"
+  [& _args]
+  (println "Welcome to issue template generator! May I have issue name? (for exit just press ENTER)")
+  (let [issue-name (s/trim (read-line))]
+    (if (= issue-name "")
+      (println "Bye!")
+      (generate-isssue-template (s/lower-case issue-name)))))
