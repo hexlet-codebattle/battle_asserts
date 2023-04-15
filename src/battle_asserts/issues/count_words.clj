d95ebf5c87c92345caacee18c3cf05859ede6f8c
vtm9 <vtmilyakov@gmail.com>
2023-04-15 10:58:25 +0300

    Try chantgpt for gen tasks

---
+++ src/battle_asserts/issues/count_words.clj
@@ -0,0 +1,33 @@
+(ns battle-asserts.issues.count-words
+  (:require [clojure.test.check.generators :as gen]
+            [faker.generate :as faker]
+            [clojure.string :as s]))
+
+(def level :elementary)
+
+(def tags ["strings" "collections"])
+
+(def description
+  {:en "Given a string, return the number of words in it."
+   :ru "Дана строка, верните количество слов в ней."})
+
+(def signature
+  {:input  [{:argument-name "str" :type {:name "string"}}]
+   :output {:type {:name "integer"}}})
+
+(defn arguments-generator []
+  (let [sentences (repeatedly 50 #(faker/sentence {:words-range [0 12]}))]
+    (gen/tuple (gen/elements sentences))))
+
+(def test-data
+  [{:expected 4 :arguments ["The quick brown fox"]}
+   {:expected 7 :arguments ["The cat in the hat is back"]}
+   {:expected 0 :arguments [""]}])
+
+
+(defn solution [str]
+  (if (s/blank? str)
+    0
+    (count (s/split str #"\s+"))))
+
+
