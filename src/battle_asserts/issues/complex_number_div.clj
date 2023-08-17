720793ec6e90e07e375cbcd2717fbc5e3218ddfe
Artem Solomatin <artem00298@gmail.com>
2023-08-18 01:04:49 +0300

    Fix number rounding (#1042)

--- src/battle_asserts/issues/complex_number_div.clj
+++ src/battle_asserts/issues/complex_number_div.clj
@@ -21,18 +21,18 @@
 
 (def test-data
   [{:expected [1 2] :arguments [[3 10] [3 1]]}
-   {:expected [12 -5] :arguments [[30 2] [2 1]]}
-   {:expected [0 -7] :arguments [[77 -11] [1 11]]}
-   {:expected [-2 -1] :arguments [[30 11] [-12 1]]}])
+   {:expected [12 -6] :arguments [[30 2] [2 1]]}
+   {:expected [-1 -8] :arguments [[77 -11] [1 11]]}
+   {:expected [-3 -2] :arguments [[30 11] [-12 1]]}])
 
 (defn solution [[real1 img1] [real2 img2]]
   (try
     (letfn [(real-part [first-pair second-pair]
-              (int  (/ (+ (* (first first-pair) (first second-pair)) (* (last first-pair) (last second-pair)))
-                       (+ (* (first second-pair) (first second-pair)) (* (last second-pair) (last second-pair))))))
+              (int (Math/floor (/ (+ (* (first first-pair) (first second-pair)) (* (last first-pair) (last second-pair)))
+                                  (+ (* (first second-pair) (first second-pair)) (* (last second-pair) (last second-pair)))))))
             (imaginary-part [first-pair second-pair]
-              (int (/ (- (* (last first-pair) (first second-pair)) (* (first first-pair) (last second-pair)))
-                      (+ (* (first second-pair) (first second-pair)) (* (last second-pair) (last second-pair))))))]
+              (int (Math/floor (/ (- (* (last first-pair) (first second-pair)) (* (first first-pair) (last second-pair)))
+                                  (+ (* (first second-pair) (first second-pair)) (* (last second-pair) (last second-pair)))))))]
       (let
        [real (real-part [real1 img1] [real2 img2])
         img  (imaginary-part [real1 img1] [real2 img2])]
