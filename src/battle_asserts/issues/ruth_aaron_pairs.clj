8936f1569285298087704a0d31cf792957b91bc4
Artem Solomatin <artem00298@gmail.com>
2023-09-12 00:23:17 +0300

    Fix ruth aaron pairs issue (#1049)

--- src/battle_asserts/issues/ruth_aaron_pairs.clj
+++ src/battle_asserts/issues/ruth_aaron_pairs.clj
@@ -6,14 +6,16 @@
 (def tags ["math"])
 
 (def description
-  {:en "Check if a given pair of numbers is a Ruth–Aaron pair.
+  {:en "Check if a given number is a Ruth–Aaron pair.
         A Ruth–Aaron pair consists of two consecutive integers (e.g. 714 and 715) for
         which the sums of the prime factors of each integer are equal.
-        When calculating the sum, take into account the repeating factors."
-   :ru "Проверьте, является ли данная пара чисел парой Рута — Аарона.
+        When calculating the sum, take into account the repeating factors.
+        If a number cannot be decomposed into prime multipliers, consider the factorization of this number to be 0."
+   :ru "Проверьте, является ли переданное число и следующее за ним, парой Рута — Аарона.
         Пара Рута — Аарона состоит из двух последовательных целых чисел (например 714 и 715)
         суммы простых множителей которых равны.
-        При подсчете суммы учитывайте повторение множителей."})
+        При подсчете суммы учитывайте повторение множителей.
+        Если число нельзя разложить на простые множители, то считайте факторизацию этого числа равной 0."})
 
 (def ruth-aaron-pairs [[5 6] [8 9] [15 16] [77 78] [125 126] [714 715] [948 949] [1330 1331] [1520 1521]
                        [1862 1863] [2491 2492] [3248 3249] [4185 4186] [4191 4192] [5405 5406] [5560 5561]
@@ -21,22 +23,18 @@
                        [16932 16933] [17080 17081] [18490 18491] [20450 20451] [24895 24896] [26642 26643] [26649 26650]])
 
 (def signature
-  {:input  [{:argument-name "arr" :type {:name "array" :nested {:name "integer"}}}]
+  {:input  [{:argument-name "num" :type {:name "integer"}}]
    :output {:type {:name "boolean"}}})
 
 (defn arguments-generator []
-  (gen/tuple (gen/one-of [(gen/elements ruth-aaron-pairs)
-                          (gen/bind gen/nat #(gen/return [%, (inc %)]))])))
+  (gen/tuple (gen/choose 2 30000)))
 
 (def test-data
-  [{:expected true
-    :arguments [[8 9]]}
-   {:expected true
-    :arguments [[5 6]]}
-   {:expected true
-    :arguments [[77 78]]}
-   {:expected false
-    :arguments [[20 21]]}])
+  [{:expected true :arguments [8]}
+   {:expected true :arguments [5]}
+   {:expected true :arguments [77]}
+   {:expected true :arguments [125]}
+   {:expected false :arguments [20]}])
 
 (defn- factorization [value [first-prime & rest-primes :as primes]]
   (cond
@@ -51,7 +49,8 @@
 (defn prime-factorization [value]
   (factorization value (primes-up-to value)))
 
-(defn solution [[value1 value2]]
-  (and (= (inc value1) value2)
-       (= (apply + (prime-factorization value1))
-          (apply + (prime-factorization value2)))))
+(defn solution [num]
+  (let [next-num (inc num)]
+    (and (= (inc num) next-num)
+         (= (apply + (prime-factorization num))
+            (apply + (prime-factorization next-num))))))
