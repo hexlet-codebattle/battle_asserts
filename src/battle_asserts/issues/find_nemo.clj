2bfb34bf2d370ce9ac28c358d0d18416262eca9e
Artem Solomatin <artem00298@gmail.com>
2024-10-11 01:32:06 +0300

    Update find_nemo.clj

--- src/battle_asserts/issues/find_nemo.clj
+++ src/battle_asserts/issues/find_nemo.clj
@@ -5,7 +5,7 @@
 
 (def level :elementary)
 
-(def tags ["strings"])
+(def tags ["strings" "games"])
 
 (def description
   {:en "You are given a string of words. You need to find the word `Nemo`, and return a string like this: \"I found Nemo at [the order of the word you find nemo]!\". If you can't find Nemo, return \"I can't find Nemo :(\"."
